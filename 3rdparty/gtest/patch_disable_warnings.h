e814e85435361a45e7abb30c6b061779d56e0eba
Kirill Kornyakov <kirill.kornyakov@itseez.com>
2016-03-07 09:30:49 +0300

    Try to enable warnings back

--- 3rdparty/gtest/patch_disable_warnings.h
+++ 3rdparty/gtest/patch_disable_warnings.h
@@ -6,10 +6,10 @@
 #endif
 
 #if defined(__clang__)
-    #pragma clang diagnostic ignored "-Wweak-vtables"
-    #pragma clang diagnostic ignored "-Wglobal-constructors"
-    #pragma clang diagnostic ignored "-Wused-but-marked-unused"
-    #pragma clang diagnostic ignored "-Wshift-sign-overflow"
-    #pragma clang diagnostic ignored "-Wc++98-compat-pedantic"
+    // #pragma clang diagnostic ignored "-Wweak-vtables"
+    // #pragma clang diagnostic ignored "-Wglobal-constructors"
+    // #pragma clang diagnostic ignored "-Wused-but-marked-unused"
+    // #pragma clang diagnostic ignored "-Wshift-sign-overflow"
+    // #pragma clang diagnostic ignored "-Wc++98-compat-pedantic"
     #pragma clang diagnostic ignored "-Wunused-const-variable"
 #endif
