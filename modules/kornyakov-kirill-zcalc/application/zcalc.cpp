4b93ec99d3425cfdc6cab5fff8cb2df910106f82
Kirill Kornyakov <kirill.kornyakov@itseez.com>
2016-03-20 12:39:53 +0300

    Minor fixes

---
+++ modules/kornyakov-kirill-zcalc/application/zcalc.cpp
@@ -0,0 +1,15 @@
+// Copyright 2016 Kornyakov Kirill
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+#include "include/complex_calculator.h"
+
+int main(int argc, const char** argv) {
+    ComplexCalculator app;
+    std::string output = app(argc, argv);
+    printf("%s\n", output.c_str());
+
+    return 0;
+}
