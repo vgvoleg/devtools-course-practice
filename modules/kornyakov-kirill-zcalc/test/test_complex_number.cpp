bd4af1cb22d557d1f2515cd19b7f536c9cac0e30
Kirill Kornyakov <kirill.kornyakov@itseez.com>
2016-03-20 15:00:09 +0300

    Refactor tests

--- modules/kornyakov-kirill-zcalc/test/test_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_complex_number.cpp
@@ -12,141 +12,113 @@ class ComplexTest : public ::testing::Test {
 };
 
 TEST_F(ComplexTest, Can_Create_With_Real_And_Imaginary) {
-    // Arrange & Act
-    double testReal = 26.0;
-    double testImaginary = 20.0;
-    ComplexNumber test(testReal, testImaginary);
+    // Arrange
+    double re = 26.0;
+    double im = 20.0;
+
+    // Act
+    ComplexNumber z(re, im);
 
     // Assert
-    EXPECT_NEAR(testReal, test.getRe(), ComplexTest::epsilon);
-    EXPECT_NEAR(testImaginary, test.getIm(), ComplexTest::epsilon);
+    EXPECT_NEAR(re, z.getRe(), ComplexTest::epsilon);
+    EXPECT_NEAR(im, z.getIm(), ComplexTest::epsilon);
 }
 
 TEST_F(ComplexTest, Can_Create_Via_Copying) {
     // Arrange
-    double testReal = 26.0;
-    double testImaginary = 20.0;
-    ComplexNumber test(testReal, testImaginary);
+    double re = 26.0;
+    double im = 20.0;
+    ComplexNumber z(re, im);
 
     // Act
-    ComplexNumber tested = test;
+    ComplexNumber expected_z = z;
 
     // Assert
-    EXPECT_NEAR(testReal, tested.getRe(), ComplexTest::epsilon);
-    EXPECT_NEAR(testImaginary, tested.getIm(), ComplexTest::epsilon);
+    EXPECT_NEAR(re, expected_z.getRe(), ComplexTest::epsilon);
+    EXPECT_NEAR(im, expected_z.getIm(), ComplexTest::epsilon);
 }
 
 TEST_F(ComplexTest, Can_Set_Real) {
     // Arrange
-    ComplexNumber test;
-    double testReal = 20.0;
+    ComplexNumber z;
+    double re = 20.0;
 
     // Act
-    test.setRe(testReal);
+    z.setRe(re);
 
     // Assert
-    EXPECT_NEAR(testReal, test.getRe(), ComplexTest::epsilon);
+    EXPECT_NEAR(re, z.getRe(), ComplexTest::epsilon);
 }
 
 TEST_F(ComplexTest, Can_Set_Imaginary) {
     // Arrange
-    ComplexNumber test;
-    double testImaginary = 26.0;
+    ComplexNumber z;
+    double im = 26.0;
 
     // Act
-    test.setIm(testImaginary);
+    z.setIm(im);
 
     // Assert
-    EXPECT_NEAR(testImaginary, test.getIm(), ComplexTest::epsilon);
+    EXPECT_NEAR(im, z.getIm(), ComplexTest::epsilon);
 }
 
 TEST_F(ComplexTest, Can_Add_Complex) {
     // Arrange
-    double testReal = 26.0;
-    double testImaginary = 14.0;
-
-    double testedReal = 1.0;
-    double testedImaginary = 7.0;
-
-    ComplexNumber test(testReal, testImaginary);
-    ComplexNumber tested(testedReal, testedImaginary);
+    ComplexNumber z1(26.0, 14.0);
+    ComplexNumber z2(1.0, 7.0);
 
     // Act
-    tested = tested + test;
+    ComplexNumber z = z1 + z2;
 
     // Assert
-    EXPECT_NEAR(27.0, tested.getRe(), ComplexTest::epsilon);
-    EXPECT_NEAR(21.0, tested.getIm(), ComplexTest::epsilon);
+    EXPECT_NEAR(27.0, z.getRe(), ComplexTest::epsilon);
+    EXPECT_NEAR(21.0, z.getIm(), ComplexTest::epsilon);
 }
 
 TEST_F(ComplexTest, Can_Difference_Complex) {
     // Arrange
-    double testReal = 26.0;
-    double testImaginary = 14.0;
-
-    double testedReal = 1.0;
-    double testedImaginary = 1.0;
-
-    ComplexNumber test(testReal, testImaginary);
-    ComplexNumber tested(testedReal, testedImaginary);
+    ComplexNumber z1(26.0, 14.0);
+    ComplexNumber z2(1.0, 1.0);
 
     // Act
-    tested = tested - test;
+    ComplexNumber z = z2 - z1;
 
     // Assert
-    EXPECT_NEAR(-25.0, tested.getRe(), ComplexTest::epsilon);
-    EXPECT_NEAR(-13.0, tested.getIm(), ComplexTest::epsilon);
+    EXPECT_NEAR(-25.0, z.getRe(), ComplexTest::epsilon);
+    EXPECT_NEAR(-13.0, z.getIm(), ComplexTest::epsilon);
 }
 
 TEST_F(ComplexTest, Can_Multiplication_Complex) {
     // Arrange
-    double testReal = 5.0;
-    double testImaginary = 3.0;
-
-    double testedReal = 4.0;
-    double testedImaginary = 6.0;
-
-    ComplexNumber test(testReal, testImaginary);
-    ComplexNumber tested(testedReal, testedImaginary);
+    ComplexNumber z1(5.0, 3.0);
+    ComplexNumber z2(4.0, 6.0);
 
     // Act
-    tested = tested * test;
+    ComplexNumber z = z1 * z2;
 
     // Assert
-    EXPECT_NEAR(2.0, tested.getRe(), ComplexTest::epsilon);
-    EXPECT_NEAR(42.0, tested.getIm(), ComplexTest::epsilon);
+    EXPECT_NEAR(2.0,  z.getRe(), ComplexTest::epsilon);
+    EXPECT_NEAR(42.0, z.getIm(), ComplexTest::epsilon);
 }
 
 TEST_F(ComplexTest, Can_Division_Complex) {
     // Arrange
-    double testReal = 15.0;
-    double testImaginary = 20.0;
-
-    double testedReal = 27.0;
-    double testedImaginary = 30.0;
-
-    ComplexNumber test(testReal, testImaginary);
-    ComplexNumber tested(testedReal, testedImaginary);
+    ComplexNumber z1(15.0, 20.0);
+    ComplexNumber z2(27.0, 30.0);
 
     // Act
-    tested = tested / test;
+    ComplexNumber z = z2 / z1;
 
     // Assert
-    EXPECT_NEAR(1.608, tested.getRe(), ComplexTest::epsilon);
-    EXPECT_NEAR(-0.144, tested.getIm(), ComplexTest::epsilon);
+    EXPECT_NEAR(1.608,  z.getRe(), ComplexTest::epsilon);
+    EXPECT_NEAR(-0.144, z.getIm(), ComplexTest::epsilon);
 }
 
 TEST_F(ComplexTest, Do_Throw_When_Division_By_Zero) {
     // Arrange
-    double testReal = 0.0;
-    double testImaginary = 0.0;
-
-    double testedReal = 26.0;
-    double testedImaginary = 14.0;
-
-    ComplexNumber test(testReal, testImaginary);
-    ComplexNumber tested(testedReal, testedImaginary);
+    ComplexNumber z1(0.0, 0.0);
+    ComplexNumber z2(26.0, 14.0);
 
     // Act & Assert
-    EXPECT_THROW(tested / test, std::string);
+    EXPECT_THROW(z2 / z1, std::string);
 }
