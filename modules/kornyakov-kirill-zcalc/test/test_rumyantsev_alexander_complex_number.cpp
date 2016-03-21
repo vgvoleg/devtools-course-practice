eed00838466ffab0ab39782592a42211cc0341b4
cetygamer <cetygamer@gmail.com>
2016-03-21 22:12:07 +0300

    Added 2 more tests

--- modules/kornyakov-kirill-zcalc/test/test_rumyantsev_alexander_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_rumyantsev_alexander_complex_number.cpp
@@ -2,11 +2,36 @@
 
 #include "include/complex_number.h"
 
-TEST(Kornyakov_Kirill_ComplexNumberTest, Can_Create_Default_Complex) {
+TEST(Rumyantsev_Alexander_ComplexNumberTest, Can_Create_Default_Complex) {
 	// Arrange
 	ComplexNumber z;
 
 	// Assert
 	EXPECT_DOUBLE_EQ(0.0, z.getRe());
 	EXPECT_DOUBLE_EQ(0.0, z.getIm());
+}
+
+TEST(Rumyantsev_Alexander_ComplexNumberTest, Can_Create_From_Int) {
+	// Arrange
+	int re = 255;
+	int im = -10;
+
+	// Act
+	ComplexNumber z(re, im);
+
+	// Assert
+	EXPECT_DOUBLE_EQ(255.0, z.getRe());
+	EXPECT_DOUBLE_EQ(-10.0, z.getIm());
+}
+
+TEST(Rumyantsev_Alexander_ComplexNumberTest, Can__Do_Multiple_Assignment) {
+	// Arrange
+	ComplexNumber z1, z2, z3(1.0, 2.0);
+
+	// Act
+	z1 = z2 = z3;
+
+	// Assert
+	EXPECT_EQ(z1, z3);
+	EXPECT_EQ(z2, z3);
 }
