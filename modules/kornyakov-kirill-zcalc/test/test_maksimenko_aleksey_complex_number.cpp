174d03474d572a237c0b178f7df57b26336b1259
Alex <crimekey@mail.ru>
2016-03-26 20:07:53 +0300

    Корректировка файла согласно замечаниям Петрова Кирилла

--- modules/kornyakov-kirill-zcalc/test/test_maksimenko_aleksey_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_maksimenko_aleksey_complex_number.cpp
@@ -6,22 +6,22 @@
 
 TEST(Maksimenko_Alex_ComplexNumberTest, can_use_some_oper_with) {
     // Arrange
-    ComplexNumber z1(2, 1);
-    ComplexNumber z2(4, 1);
-    ComplexNumber z3(1, 1);
+    ComplexNumber z1(2.0, 1.0);
+    ComplexNumber z2(4.0, 1.0);
+    ComplexNumber z3(1.0, 1.0);
 
     // Act
     ComplexNumber z = (z1*z2) / z3;
 
     // Assert
-    ComplexNumber result(6.5, -0.5);
-    EXPECT_EQ(z, result);
+    ComplexNumber expected_z(6.5, -0.5);
+    EXPECT_EQ(z, expected_z);
 }
 
 TEST(Maksimenko_Alex_ComplexNumberTest, can_calc_div_dif_of_sqr_and_dif) {
     // Arrange
-    ComplexNumber z1(4, 2);
-    ComplexNumber z2(6, 8);
+    ComplexNumber z1(4.0, 2.0);
+    ComplexNumber z2(6.0, 8.0);
 
     // Act
     ComplexNumber z = (z1*z1 - z2*z2) / (z1 - z2);
@@ -33,8 +33,8 @@ TEST(Maksimenko_Alex_ComplexNumberTest, can_calc_div_dif_of_sqr_and_dif) {
 
 TEST(Maksimenko_Alex_ComplexNumberTest, can_calc_div_dif_of_sqr_and_sum) {
     // Arrange
-    ComplexNumber z1(4, 2);
-    ComplexNumber z2(6, 8);
+    ComplexNumber z1(4.0, 2.0);
+    ComplexNumber z2(6.0, 8.0);
 
     // Act
     ComplexNumber z = (z1*z1 - z2*z2) / (z1 + z2);
@@ -46,25 +46,25 @@ TEST(Maksimenko_Alex_ComplexNumberTest, can_calc_div_dif_of_sqr_and_sum) {
 
 TEST(Maksimenko_Alex_ComplexNumberTest, can_work_with_complex_conjugates) {
     // Arrange
-    ComplexNumber z1(1, -1);
-    ComplexNumber z2(1, 1);
-    ComplexNumber a(7, 0);
-    ComplexNumber b(14, 0);
-    ComplexNumber ins(-1, 0);
+    ComplexNumber z1(1.0, -1.0);
+    ComplexNumber z2(1.0, 1.0);
+    ComplexNumber a(7.0, 0.0);
+    ComplexNumber b(14.0, 0.0);
+    ComplexNumber ins(-1.0, 0.0);
 
     // Act
     // (a*(1-i)^2/(b*(1+i)^2)=-a/b)
     ComplexNumber  division = a*z1*z1 / (b*z2*z2);
 
     // Assert
-    ComplexNumber result = ins*a / b;
-    EXPECT_EQ(division, result);
+    ComplexNumber expected_z = ins*a / b;
+    EXPECT_EQ(division, expected_z);
 }
 
 TEST(Maksimenko_Alex_ComplexNumberTest, can_apply_the_form_of_diff_of_cub) {
     // Arrange
-    ComplexNumber z1(1, 1);
-    ComplexNumber z2(1, 2);
+    ComplexNumber z1(1.0, 1.0);
+    ComplexNumber z2(1.0, 2.0);
 
     // Act & Assert
     // (z1)^3-(z2)^3=((z1)^2+z1z2+(z2)^2)
