8b244835c4a33b1a9d48a68b70be78601af06cc7
Alex <crimekey@mail.ru>
2016-03-26 17:34:33 +0300

    Исправляю баги для Travis(2)

--- modules/kornyakov-kirill-zcalc/test/test_maksimenko_aleksey_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_maksimenko_aleksey_complex_number.cpp
@@ -7,69 +7,73 @@
 #include "include/complex_number.h"
 
 TEST(Maksimenko_Aleksey_ComplexNumberTest, division_and_multiplication) {
-	//Arrange
-	//a, b and c - complex numbers;
+	// Arrange
 	ComplexNumber a(2, 1);
 	ComplexNumber b(4, 1);
 	ComplexNumber c(1, 1);
 	ComplexNumber result(6.5, -0.5);
-	//Act
+
+	// Act
 	ComplexNumber z = (a*b) / c;
-	//Assert
+
+	// Assert
 	EXPECT_EQ(z,result);
 }
 
 TEST(Maksimenko_Aleksey_ComplexNumberTest, difference_of_squares_and_difference) {
-	//Arrange
-	//cn1 and cn2 - complex numbers;
+	// Arrange
 	ComplexNumber cn1(4, 2);
 	ComplexNumber cn2(6, 8);
-	//Act
-	//((4 + 2i)^2 - (6 + 8i)^2)/(4+2i-6-8i)=4+2i+6+8i;
+	
+	// Act
 	ComplexNumber result = cn1 + cn2;
 	ComplexNumber res = (cn1*cn1 - cn2*cn2) / (cn1 - cn2);
-	//Assert
+	
+	// Assert
 	EXPECT_EQ(res, result);
 }
 
 TEST(Maksimenko_Aleksey_ComplexNumberTest, difference_of_squares_and_sums) {
-	//Arrange
+	// Arrange
 	ComplexNumber cn1(4, 2);
 	ComplexNumber cn2(6, 8);
-	//Act
-	//((4 + 2i)^2 - (6 + 8i)^2)/(4+2i+6+8i)=4+2i-6-8i;
+	
+	// Act
 	ComplexNumber result = cn1 - cn2;
 	ComplexNumber res = (cn1*cn1 - cn2*cn2) / (cn1 + cn2);
-	//Assert
+	
+	// Assert
 	EXPECT_EQ(res, result);
 }
 
 TEST(Maksimenko_Aleksey_ComplexNumberTest, can_work_with_complex_numbers_conjugates)) {
-	//Arrange
+	// Arrange
 	ComplexNumber cn1(1, -1);
 	ComplexNumber cn2(1, 1);
 	ComplexNumber n1(7, 0);
 	ComplexNumber n2(14, 0);
 	ComplexNumber ins(-1, 0);
-	//Act
-	//a*(1-i)^2/(b*(1+i)^2)=a*(1+i)^2/(b*(1-i)^2)=-a/b;
+	
+	// Act
 	ComplexNumber res1 = n1*cn1*cn1 / (n2*cn2*cn2);
 	ComplexNumber res2 = n1*cn2*cn2 / (n2*cn1*cn1);
 	ComplexNumber result = ins*n1 / n2;
-	//Assert
+	
+	// Assert
 	EXPECT_EQ(res1, res2);
 	EXPECT_EQ(res1, result);
 	EXPECT_EQ(result, res2);
 }
 
 TEST(Maksimenko_Aleksey_ComplexNumberTest, can_apply_the_formula_of_difference_of_cubes) {
-	//Arrange
+	// Arrange
 	ComplexNumber cn1(1, 1);
 	ComplexNumber cn2(1, 2);
-	//Act
-	//a^3-b^3=(a-b)*(a^2+ab+b^2);
+	
+	// Act
 	ComplexNumber res=cn1*cn1*cn1-cn2*cn2*cn2;
 	ComplexNumber result = (cn1 - cn2)*(cn1*cn1 + cn1*cn2 + cn2*cn2);
-	//Assert
+	
+	// Assert
 	EXPECT_EQ(res, result);
 }
