f821b02769fda4239b05fd5e44bb6ae14a609d94
Alex <crimekey@mail.ru>
2016-03-26 18:14:03 +0300

    Исправляю баги для Travis(5)

--- modules/kornyakov-kirill-zcalc/test/test_maksimenko_aleksey_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_maksimenko_aleksey_complex_number.cpp
@@ -6,66 +6,66 @@
 
 #include "include/complex_number.h"
 
-TEST(Maksimenko_Aleksey_ComplexNumberTest, division_and_multiplication) {
+TEST(Maksimenko_Alex_ComplexNumberTest, division_and_multiplication) {
     // Arrange
     ComplexNumber a(2, 1);
     ComplexNumber b(4, 1);
     ComplexNumber c(1, 1);
     ComplexNumber result(6.5, -0.5);
 
-    // Act
+	// Act
     ComplexNumber z = (a*b) / c;
 
     // Assert
-    EXPECT_EQ(z,result);
+	EXPECT_EQ(z, result);
 }
 
-TEST(Maksimenko_Aleksey_ComplexNumberTest, difference_of_squares_and_difference) {
+TEST(Maksimenko_Alex_ComplexNumberTest, div_diff_of_squares_and_diff_complex) {
     // Arrange
     ComplexNumber cn1(4, 2);
     ComplexNumber cn2(6, 8);
-	
+
     // Act
     ComplexNumber result = cn1 + cn2;
     ComplexNumber res = (cn1*cn1 - cn2*cn2) / (cn1 - cn2);
-	
+
     // Assert
     EXPECT_EQ(res, result);
 }
 
-TEST(Maksimenko_Aleksey_ComplexNumberTest, difference_of_squares_and_sums) {
+TEST(Maksimenko_Alex_ComplexNumberTest, div_diff_of_squares_and_sums_complex) {
     // Arrange
     ComplexNumber cn1(4, 2);
     ComplexNumber cn2(6, 8);
-	
+
     // Act
     ComplexNumber result = cn1 - cn2;
     ComplexNumber res = (cn1*cn1 - cn2*cn2) / (cn1 + cn2);
-	
+
     // Assert
     EXPECT_EQ(res, result);
 }
 
-TEST(Maksimenko_Aleksey_ComplexNumberTest, can_work_with_complex_numbers_conjugates)) {
+TEST(Maksimenko_Alex_ComplexNumberTest, can_work_with_complex_conjugates)) {
     // Arrange
     ComplexNumber cn1(1, -1);
     ComplexNumber cn2(1, 1);
     ComplexNumber n1(7, 0);
     ComplexNumber n2(14, 0);
     ComplexNumber ins(-1, 0);
-	
+
     // Act
     ComplexNumber res1 = n1*cn1*cn1 / (n2*cn2*cn2);
     ComplexNumber res2 = n1*cn2*cn2 / (n2*cn1*cn1);
     ComplexNumber result = ins*n1 / n2;
-	
+
     // Assert
     EXPECT_EQ(res1, res2);
     EXPECT_EQ(res1, result);
     EXPECT_EQ(result, res2);
 }
 
-TEST(Maksimenko_Aleksey_ComplexNumberTest, can_apply_the_formula_of_difference_of_cubes) {
+TEST(Maksimenko_Alex_ComplexNumberTest, can_apply_the_formula_of_diff_of_cubes) {
     // Arrange
     ComplexNumber cn1(1, 1);
     ComplexNumber cn2(1, 2);
@@ -73,7 +73,7 @@ TEST(Maksimenko_Aleksey_ComplexNumberTest, can_apply_the_formula_of_difference_o
     // Act
     ComplexNumber res=cn1*cn1*cn1-cn2*cn2*cn2;
     ComplexNumber result = (cn1 - cn2)*(cn1*cn1 + cn1*cn2 + cn2*cn2);
-	
+
     // Assert
     EXPECT_EQ(res, result);
 }
