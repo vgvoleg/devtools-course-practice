1f9604d10d08ffbf406a7d645d5345d92f5b5a6a
Артемчик Земцов < >
2016-03-27 23:07:21 +0300

    add 3 tests

--- modules/kornyakov-kirill-zcalc/test/test_zemtsov_artem_copmplex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_zemtsov_artem_copmplex_number.cpp
@@ -1,7 +1,6 @@
 // Copyright 2016 Zemtsov Artem
 
 #include <gtest/gtest.h>
-
 #include "include/complex_number.h"
 
 TEST(Zemtsov_Artem_ComplexNumberTest, Can_Get_Sum_Of_The_Many_Complex) {
@@ -18,6 +17,7 @@ TEST(Zemtsov_Artem_ComplexNumberTest, Can_Get_Sum_Of_The_Many_Complex) {
 	ComplexNumber expected_z(10000.0, 10000.0);
 	EXPECT_EQ(expected_z, z_res);
 }
+
 TEST(Zemtsov_Artem_ComplexNumberTest, distributivity_is_work) {
 	// Arrange
 	ComplexNumber firstEx(10.0, 10.0);
@@ -26,4 +26,37 @@ TEST(Zemtsov_Artem_ComplexNumberTest, distributivity_is_work) {
 
 	// Normal human assert
 	EXPECT_EQ(firstEx*thirdEx+secondEx*thirdEx, thirdEx*(firstEx+secondEx));
+}
+
+TEST(Zemtsov_Artem_ComplexNumberTest, check_conjuate_Is_Abs_Square) {
+	// Arrange
+	ComplexNumber firstEx(8.0, 6.0), secEx(8.0, -6.0);
+
+	// Act
+	ComplexNumber multy = firstEx * secEx;
+	double abs = sqrt(firstEx.getRe() * firstEx.getRe() + firstEx.getIm() * firstEx.getIm());
+	ComplexNumber absSquare(abs * abs, 0.0);
+
+	// Assert
+	EXPECT_TRUE(absSquare == multy);
+}
+
+TEST(Zemtsov_Artem_ComplexNumberTest, can_get_and_set_re_and_im) {
+	//Arrange
+	ComplexNumber firstEx(13,66),secEx;
+
+	//Act
+	secEx.setIm(firstEx.getIm());
+	secEx.setRe(firstEx.getRe());
+
+	//Assert
+    EXPECT_EQ(firstEx, secEx);
+}
+
+TEST(Zemtsov_Artem_ComplexNumberTest, checking_of_the_inequality_operator) {
+	//Arrange
+	ComplexNumber firstEx(13, 66), secEx(66,13);
+
+	//Assert
+	EXPECT_NE(firstEx, secEx);
 }
