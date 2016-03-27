e5f372a3646f324cfd01687aadc44eeae561e47f
Maria <essamirra@live.com>
2016-03-27 20:28:30 +0300

    added second test

--- modules/kornyakov-kirill-zcalc/test/test_pronina_maria_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_pronina_maria_complex_number.cpp
@@ -14,3 +14,15 @@ TEST(Pronina_Maria_ComplexNumberTest,
     ComplexNumber res(-1.0, 0.0);
     EXPECT_TRUE(res == square);
 }
+
+TEST(Pronina_Maria_ComplexNumberTest, Triangle_Inequality_For_Complex) {
+    // Arrange
+    ComplexNumber z1(6.0, 5.0), z2(7.0, 8.0);
+    //Act
+    ComplexNumber sum = z1 + z2;
+    double sumAbs = sqrt(sum.getRe() * sum.getRe() + sum.getIm() * sum.getIm());
+    double z1Abs = sqrt(z1.getRe() * z1.getRe() + z1.getIm() * z1.getIm());
+    double z2Abs = sqrt(z2.getRe() * z2.getRe() + z2.getIm() * z2.getIm());
+    // Assert
+    EXPECT_TRUE(sumAbs < z1Abs + z2Abs);
+}
