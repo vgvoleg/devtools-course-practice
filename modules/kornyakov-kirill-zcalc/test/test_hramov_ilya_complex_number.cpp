2e6ec0c853d1c1cb9263791d05e4b21ffbafc3d8
Ilya Hramov <sinobi67@gmail.com>
2016-03-27 22:55:38 +0300

    Fix commets style

--- modules/kornyakov-kirill-zcalc/test/test_hramov_ilya_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_hramov_ilya_complex_number.cpp
@@ -1,58 +1,59 @@
 // Copyright 2016 Hramov Ilya
 
-#include <gtest\gtest.h>
-#include "include\complex_number.h"
+#include <gtest/gtest.h>
+
+#include "include/complex_number.h"
 
 TEST(Hramov_Ilya_ComplexNumberTest, Can_Compare_For_Equality) {
-    //Arrange
+    // Arrange
     ComplexNumber z1(1.0, 2.0), z2(1.0, 2.0), z3(1.1, 2.0);
 
-    //Assert
+    // Assert
     EXPECT_TRUE(z1 == z2);
     EXPECT_FALSE(z1 == z3);
 }
 
 TEST(Hramov_Ilya_ComplexNumberTest, Can_Compare_For_Inequality) {
-    //Arrange
+    // Arrange
     ComplexNumber z1(1.0, 2.0), z2(1.0, 2.0), z3(1.1, 2.0);
 
-    //Assert
+    // Assert
     EXPECT_TRUE(z1 != z3);
     EXPECT_FALSE(z1 != z2);
 }
 
 TEST(Hramov_Ilya_ComplexNumberTest, Can_Multiply_Not_Extraordinnary_Numbers) {
-    //Arrange
+    // Arrange
     ComplexNumber z1(1.0, 2.0), z2(3.0, 4.0);
 
-    //Act
+    // Act
     ComplexNumber result = z1 * z2;
 
-    //Assert
+    // Assert
     EXPECT_DOUBLE_EQ(-5.0, result.getRe());
     EXPECT_DOUBLE_EQ(10.0, result.getIm());
 }
 
 TEST(Hramov_Ilya_ComplexNumberTest, Can_Add_Not_Extraordinnary_Numbers) {
-    //Arrange
+    // Arrange
     ComplexNumber z1(1.0, 2.0), z2(3.0, 4.0);
 
-    //Act
+    // Act
     ComplexNumber result = z1 + z2;
 
-    //Assert
+    // Assert
     EXPECT_DOUBLE_EQ(4.0, result.getRe());
     EXPECT_DOUBLE_EQ(6.0, result.getIm());
 }
 
 TEST(Hramov_Ilya_ComplexNumberTest, Can_Subtract_Not_Extraordinnary_Numbers) {
-    //Arrange
+    // Arrange
     ComplexNumber z1(1.0, 2.0), z2(3.0, 4.0);
 
-    //Act
+    // Act
     ComplexNumber result = z1 - z2;
 
-    //Assert
+    // Assert
     EXPECT_DOUBLE_EQ(-2.0, result.getRe());
     EXPECT_DOUBLE_EQ(-2.0, result.getIm());
 }
