06fbf322477e1b67a388e666f1d60870e512935f
Vlad Koshechkin <koshechkin-vlad@yandex.ru>
2016-03-27 22:41:07 +0300

    add 5 tests

--- modules/kornyakov-kirill-zcalc/test/test_koshechkin_vlad_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_koshechkin_vlad_complex_number.cpp
@@ -1,13 +1,9 @@
-//
-// Created by Влад on 27.03.16.
-//
-
+// Copyright 2016 Koshechkin Vlad
 
 #include <gtest/gtest.h>
 
 #include "include/complex_number.h"
-using std::vector;
-using std::string;
+
 
 TEST(Koshechkin_Vlad_ComplexNumberTest, Can_Create_With_Zero_Imaginary) {
     // Arrange
@@ -18,18 +14,55 @@ TEST(Koshechkin_Vlad_ComplexNumberTest, Can_Create_With_Zero_Imaginary) {
     ComplexNumber z(re, im);
 
     // Assert
-    EXPECT_EQ(re, z.getRe());
+    EXPECT_DOUBLE_EQ(re, z.getRe());
     EXPECT_DOUBLE_EQ(im, z.getIm());
 }
+
 TEST(Koshechkin_Vlad_ComplexNumberTest, Can_Assignment) {
     // Arrange
     ComplexNumber z;
     ComplexNumber c(1.0, 2.0);
 
     // Act
-    z=c;
+    z = c;
 
     // Assert
     EXPECT_DOUBLE_EQ(1.0, z.getRe());
     EXPECT_DOUBLE_EQ(2.0, z.getIm());
+}
+
+TEST(Koshechkin_Vlad_ComplexNumberTest, Summ_Zero) {
+    // Arrange
+    ComplexNumber z;
+    ComplexNumber c;
+
+    // Act
+    ComplexNumber v = z + c;
+
+    // Assert
+    EXPECT_DOUBLE_EQ(0.0, v.getRe());
+    EXPECT_DOUBLE_EQ(0.0, v.getIm());
+}
+
+TEST(Koshechkin_Vlad_ComplexNumberTest, Can_Copy_Number) {
+    // Arrange
+    ComplexNumber z(1.0, 2.0);
+
+    // Act
+    ComplexNumber c(z);
+
+    // Assert
+    EXPECT_EQ(c, z);
+}
+
+TEST(Koshechkin_Vlad_ComplexNumberTest, Can_Multiply_One) {
+    // Arrange
+    ComplexNumber z(1.0, 2.0);
+    ComplexNumber c(1.0, 0.0);
+
+    // Act
+    ComplexNumber v = z * c;
+
+    // Assert
+    EXPECT_EQ(v, z);
 }
