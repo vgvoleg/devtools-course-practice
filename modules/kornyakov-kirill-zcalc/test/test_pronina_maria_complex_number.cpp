a5a666efe3dee6364a726ce0a786d56768c3fc82
Maria <essamirra@live.com>
2016-03-27 20:51:54 +0300

    change formatting again

--- modules/kornyakov-kirill-zcalc/test/test_pronina_maria_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_pronina_maria_complex_number.cpp
@@ -1,15 +1,15 @@
 // Copyright 2016 Pronina Maria
-#include "include/complex_number.h"
 #include <math.h>
 #include <gtest/gtest.h>
+#include "include/complex_number.h"
 
 TEST(Pronina_Maria_ComplexNumberTest,
     Imaginary_Number_Square_Is_Negative_Real) {
      // Arrange
     ComplexNumber z(0.0, 1.0);
-     //Act
+     // Act
     ComplexNumber square = z * z;
-     // Assert 
+     // Assert
     ComplexNumber res(-1.0, 0.0);
     EXPECT_TRUE(res == square);
 }
@@ -17,7 +17,7 @@ TEST(Pronina_Maria_ComplexNumberTest,
 TEST(Pronina_Maria_ComplexNumberTest, Triangle_Inequality_For_Complex) {
      // Arrange
     ComplexNumber z1(6.0, 5.0), z2(7.0, 8.0);
-     //Act
+     // Act
     ComplexNumber sum = z1 + z2;
     double sumAbs = sqrt(sum.getRe() * sum.getRe() + sum.getIm() * sum.getIm());
     double z1Abs = sqrt(z1.getRe() * z1.getRe() + z1.getIm() * z1.getIm());
@@ -30,7 +30,7 @@ TEST(Pronina_Maria_ComplexNumberTest,
     Mult_Of_Complex_And_Conjuate_Is_Abs_Square) {
      // Arrange
     ComplexNumber z1(8.0, 6.0), z2(8.0, -6.0);
-     //Act
+     // Act
     ComplexNumber mult = z1 * z2;
      // Assert
     double z1Abs = sqrt(z1.getRe() * z1.getRe() + z1.getIm() * z1.getIm());
@@ -42,7 +42,7 @@ TEST(Pronina_Maria_ComplexNumberTest,
     Conjuate_Of_Mult_Is_Equal_To_Mul_Of_Conjuates) {
      // Arrange
     ComplexNumber z1(8.0, 6.0), z2(4.0, -3.0);
-     //Act
+     // Act
     ComplexNumber mult = z1 * z2;
     ComplexNumber multConj(mult.getRe(), -mult.getIm());
      // Assert
@@ -54,7 +54,7 @@ TEST(Pronina_Maria_ComplexNumberTest,
     Conjuate_Of_Division_Is_Equal_To_Division_Of_Conjuates) {
      // Arrange
     ComplexNumber z1(8.0, 6.0), z2(4.0, -3.0);
-     //Act
+     // Act
     ComplexNumber div = z1 / z2;
     ComplexNumber divtConj(div.getRe(), -div.getIm());
      // Assert
