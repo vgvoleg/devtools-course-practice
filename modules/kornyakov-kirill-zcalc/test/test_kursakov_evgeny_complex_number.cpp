0009be93bb0a84b0b800a91dc1ebe8f79ec93a03
Evgeny Kursakov <ekursakov@yandex.ru>
2016-03-27 22:13:44 +0300

    Fixed line lengths

--- modules/kornyakov-kirill-zcalc/test/test_kursakov_evgeny_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_kursakov_evgeny_complex_number.cpp
@@ -4,7 +4,8 @@
 
 #include "include/complex_number.h"
 
-TEST(Kursakov_Evgeny_ComplexNumberTest, Multiplication_Of_Complex_Number_And_Conjugate_Is_Positive_Real) {
+TEST(Kursakov_Evgeny_ComplexNumberTest,
+     Multiplication_Of_Complex_Number_And_Conjugate_Is_Positive_Real) {
     // Arrange
     ComplexNumber z1(2.0, 3.0);
     ComplexNumber z2(2.0, -3.0);
@@ -17,7 +18,8 @@ TEST(Kursakov_Evgeny_ComplexNumberTest, Multiplication_Of_Complex_Number_And_Con
     EXPECT_EQ(z3, expected_z);
 }
 
-TEST(Kursakov_Evgeny_ComplexNumberTest, Sum_Of_Number_And_Conjugate_Is_Real) {
+TEST(Kursakov_Evgeny_ComplexNumberTest,
+     Sum_Of_Number_And_Conjugate_Is_Real) {
     // Arrange
     ComplexNumber z1(26.0, 14.0);
     ComplexNumber z2(26.0, -14.0);
@@ -30,7 +32,8 @@ TEST(Kursakov_Evgeny_ComplexNumberTest, Sum_Of_Number_And_Conjugate_Is_Real) {
     EXPECT_EQ(z3, expected_z);
 }
 
-TEST(Kursakov_Evgeny_ComplexNumberTest, Multiplication_Is_Commutative) {
+TEST(Kursakov_Evgeny_ComplexNumberTest,
+     Multiplication_Is_Commutative) {
     // Arrange
     ComplexNumber z1(26.0, 14.0);
     ComplexNumber z2(1.0, 7.0);
@@ -43,7 +46,8 @@ TEST(Kursakov_Evgeny_ComplexNumberTest, Multiplication_Is_Commutative) {
     EXPECT_EQ(z3, z4);
 }
 
-TEST(Kursakov_Evgeny_ComplexNumberTest, Add_Is_Commutative) {
+TEST(Kursakov_Evgeny_ComplexNumberTest,
+     Add_Is_Commutative) {
     // Arrange
     ComplexNumber z1(26.0, 14.0);
     ComplexNumber z2(1.0, 7.0);
@@ -56,7 +60,8 @@ TEST(Kursakov_Evgeny_ComplexNumberTest, Add_Is_Commutative) {
     EXPECT_EQ(z3, z4);
 }
 
-TEST(Kursakov_Evgeny_ComplexNumberTest, Multiplication_Of_Number_And_Reciprocal_Is_Equals_One) {
+TEST(Kursakov_Evgeny_ComplexNumberTest,
+     Multiplication_Of_Number_And_Reciprocal_Is_Equals_One) {
     // Arrange
     ComplexNumber z(26.0, 14.0);
     ComplexNumber one(1.0, 0.0);
