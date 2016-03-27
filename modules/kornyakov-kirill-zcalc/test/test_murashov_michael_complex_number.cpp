ffef5d2dba6d73d9eb2b31f6385f45f1e4bd4fe1
Murashov_Michael <happy-est@yandex.ru>
2016-03-27 16:54:15 +0300

    file fixed v2 for travis

--- modules/kornyakov-kirill-zcalc/test/test_murashov_michael_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_murashov_michael_complex_number.cpp
@@ -5,7 +5,7 @@
 #include "include/complex_number.h"
 
 TEST(Murashov_Michael_ComplexNumberTest, can_work_with_public_methods) {
-	// Arrange
+    // Arrange
     double re = 2.0;
     double im = 3.0;
     ComplexNumber z;
@@ -36,18 +36,18 @@ TEST(Murashov_Michael_ComplexNumberTest, can_count_module) {
     // Arrange
     ComplexNumber z1(2.0, -1.0);
 
-    // Act 
+    // Act
     double acr1 = pow(z1.getIm(), 2);
     double acr2 = pow(z1.getRe(), 2);
     double vect = sqrt(acr1 + acr2);
 
-    // Assert 
+    // Assert
     double expected_vect = sqrt(4.0 + 1.0);
 
     EXPECT_EQ(expected_vect, vect);
 }
 
-TEST(Murashov_Michael_ComplexNumberTest, ñan_do_algebraic_operations) {
+TEST(Murashov_Michael_ComplexNumberTest, can_do_algebraic_operations) {
     // Arrange
     ComplexNumber z1(1.0, -1.0);
     ComplexNumber z2(2.0, 2.0);
@@ -63,14 +63,14 @@ TEST(Murashov_Michael_ComplexNumberTest, ñan_do_algebraic_operations) {
     EXPECT_EQ(expected_z, z);
 }
 
-TEST(Murashov_Michael_ComplexNumberTest, DISABLED_correct_cube_of_imaginary_number) {
+TEST(Murashov_Michael_ComplexNumberTest, DISABLED_correct_cube_of_im_num) {
     // Arrange
     ComplexNumber z(0.0, 1.0);
 
-    ComplexNumber cube; // z^3
+    ComplexNumber cube;  // z^3
 
     // Act
-    cube = z * z * z; // re = 0, im = -1
+    cube = z * z * z;  // re = 0, im = -1
 
     // Assert
     ComplexNumber expected_z(0.0, -1.0);
