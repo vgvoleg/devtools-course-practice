82b68b7e6448071b546472e27800a16b259de29c
Murashov_Michael <happy-est@yandex.ru>
2016-03-27 13:39:48 +0300

    add test file

---
+++ modules/kornyakov-kirill-zcalc/test/test_murashov_michael_complex_number.cpp
@@ -0,0 +1,30 @@
+/* Copyright 2016 Murashov Michael© */
+
+#include <gtest/gtest.h>
+
+#include "include/complex_number.h"
+
+TEST(Murashov_Michael_ComplexNumberTest, ) 
+{
+	
+}
+
+TEST(Murashov_Michael_ComplexNumberTest, )
+{
+
+}
+
+TEST(Murashov_Michael_ComplexNumberTest, )
+{
+
+}
+
+TEST(Murashov_Michael_ComplexNumberTest, )
+{
+
+}
+
+TEST(Murashov_Michael_ComplexNumberTest, )
+{
+
+}
