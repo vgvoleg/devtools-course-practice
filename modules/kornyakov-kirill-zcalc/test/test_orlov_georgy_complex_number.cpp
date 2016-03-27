96c6c451c2697e390fac4fb2a8d64735ad4ba5a7
Georgy <georg3796@gmail.com>
2016-03-27 17:45:35 +0300

    Fix tabs and lines<=80

--- modules/kornyakov-kirill-zcalc/test/test_orlov_georgy_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_orlov_georgy_complex_number.cpp
@@ -4,70 +4,70 @@
 
 #include <include\complex_number.h>
 
-TEST(Orlov_Georgy_ComplexNumberTest, squared_imaginary_number_is_equal_minus_one) {
-	// Arrange
-	const ComplexNumber z1(0, 1);
-	const ComplexNumber z2(-1, 0);
-	
-	// Act
-	ComplexNumber result = z1 * z1;
+TEST(Orlov_Georgy_ComplexNumberTest, squared_img_number_equal_minus_one) {
+    // Arrange
+    const ComplexNumber z1(0, 1);
+    const ComplexNumber z2(-1, 0);
+    
+    // Act
+    ComplexNumber result = z1 * z1;
 
-	// Assert
-	EXPECT_EQ(z2, result);
+    // Assert
+    EXPECT_EQ(z2, result);
 }
 
-TEST(Orlov_Georgy_ComplexNumberTest, the_order_of_addition_does_not_affect_the_result) {
-	// Arrange
-	const ComplexNumber z1(10, 15);
-	const ComplexNumber z2(6, 12);
+TEST(Orlov_Georgy_ComplexNumberTest, addition_is_commutative) {
+    // Arrange
+    const ComplexNumber z1(10, 15);
+    const ComplexNumber z2(6, 12);
 
-	// Act
-	ComplexNumber result1 = z1 + z2;
-	ComplexNumber result2 = z2 + z1;
+    // Act
+    ComplexNumber result1 = z1 + z2;
+    ComplexNumber result2 = z2 + z1;
 
-	// Assert
-	EXPECT_EQ(result1, result2);
+    // Assert
+    EXPECT_EQ(result1, result2);
 }
 
-TEST(Orlov_Georgy_ComplexNumberTest, the_order_of_multiplication_does_not_affect_the_result) {
-	// Arrange
-	const ComplexNumber z1(10, 15);
-	const ComplexNumber z2(6, 12);
+TEST(Orlov_Georgy_ComplexNumberTest, multiplication_is_commutative) {
+    // Arrange
+    const ComplexNumber z1(10, 15);
+    const ComplexNumber z2(6, 12);
 
-	// Act
-	ComplexNumber result1 = z1 * z2;
-	ComplexNumber result2 = z2 * z1;
+    // Act
+    ComplexNumber result1 = z1 * z2;
+    ComplexNumber result2 = z2 * z1;
 
-	// Assert
-	EXPECT_EQ(result1, result2);
+    // Assert
+    EXPECT_EQ(result1, result2);
 }
 
-TEST(Orlov_Georgy_ComplexNumberTest, large_expression_with_all_ops_gives_the_correct_result) {
-	// Arrange
-	const ComplexNumber z1(2, 4);
-	const ComplexNumber z2(8, 12);
-	const ComplexNumber z3(-6, 5);
-	const ComplexNumber z4(8, 12);
-	const ComplexNumber z5(1, -1);
-	const ComplexNumber expected(-36, 51);
+TEST(Orlov_Georgy_ComplexNumberTest, expr_with_all_ops_gives_the_correct_result) {
+    // Arrange
+    const ComplexNumber z1(2, 4);
+    const ComplexNumber z2(8, 12);
+    const ComplexNumber z3(-6, 5);
+    const ComplexNumber z4(8, 12);
+    const ComplexNumber z5(1, -1);
+    const ComplexNumber expected(-36, 51);
 
-	// Act
-	ComplexNumber result = z1 * z2 + z3 - z4 / z5;
+    // Act
+    ComplexNumber result = z1 * z2 + z3 - z4 / z5;
 
-	// Assert
-	EXPECT_EQ(result, expected);
+    // Assert
+    EXPECT_EQ(result, expected);
 }
 
 TEST(Orlov_Georgy_ComplexNumberTest, distributivity_check) {
-	// Arrange
-	const ComplexNumber z1(-2, 5);
-	const ComplexNumber z2(6, 4);
-	const ComplexNumber z3(8, 1);
+    // Arrange
+    const ComplexNumber z1(-2, 5);
+    const ComplexNumber z2(6, 4);
+    const ComplexNumber z3(8, 1);
 
-	// Act
-	ComplexNumber result1 = z1 * (z2 + z3);
-	ComplexNumber result2 = (z1 * z2) + (z1 * z3);
+    // Act
+    ComplexNumber result1 = z1 * (z2 + z3);
+    ComplexNumber result2 = (z1 * z2) + (z1 * z3);
 
-	// Assert
-	EXPECT_EQ(result1, result2);
+    // Assert
+    EXPECT_EQ(result1, result2);
 }
