4c941960c874e9aff9dbf6e9aae29191d7432c69
Andrey <andrey-95nn@yandex.ru>
2016-03-27 14:07:34 +0300

    Added copyright and 3 new tests.

--- modules/kornyakov-kirill-zcalc/test/test_marchenko_andrey.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_marchenko_andrey.cpp
@@ -1,3 +1,5 @@
+// Copyright 2016 Marchenko Andrey
+
 #include <gtest/gtest.h>
 #include "cmath"
 #include "include/complex_number.h"
@@ -15,7 +17,7 @@ TEST(Marchenko_Andrey_ComplexNumberTest, Can_Create_Int_Char_Variable) {
     EXPECT_EQ(im, z.getIm());
 }
 
-TEST(Marchenko_Andrey_ComplexNumberTest, Can_Use_Big_Complex_Numbers) {
+TEST(Marchenko_Andrey_ComplexNumberTest, Can_Add_Big_Complex_Numbers) {
     // Arrange
     double a = sqrt(1000);
     double b = sqrt(2000);
@@ -29,3 +31,50 @@ TEST(Marchenko_Andrey_ComplexNumberTest, Can_Use_Big_Complex_Numbers) {
     // Assert
     EXPECT_EQ(result, expected_result);
 }
+
+TEST(Marchenko_Andrey_ComplexNumberTest, Ñheck_The_Triangle_Inequality) {
+    // Arrange
+    ComplexNumber z1(1, 2);
+    ComplexNumber z2(3, 4);
+
+    // Act
+    ComplexNumber result_sum = z1 + z2;
+    double abs_of_sum = hypot(result_sum.getRe(), result_sum.getIm());
+    double sum_of_abs = hypot(z1.getRe(), z1.getIm()) + hypot(z2.getRe(), z2.getIm());
+    
+    // Assert
+    EXPECT_LE(abs_of_sum, sum_of_abs);
+}
+
+TEST(Marchenko_Andrey_ComplexNumberTest, Multiplication_Is_Distibutive) {
+    // Arrange
+    ComplexNumber z1(1, 2);
+    ComplexNumber z2(3, 4);
+    ComplexNumber z3(5, 6);
+
+    // Act
+    ComplexNumber first_result = (z1 + z2)*z3;
+    ComplexNumber second_result = z1*z3+z2*z3;
+
+    // Assert
+    EXPECT_EQ(first_result, second_result);
+}
+
+TEST(Marchenko_Andrey_ComplexNumberTest, Summation_Is_Commutative) {
+    // Arrange
+    ComplexNumber z1(1, 2);
+    ComplexNumber z2(3, 4);
+
+    // Act
+    ComplexNumber first_result = z1 + z2;
+    ComplexNumber second_result = z2 + z1;
+
+    // Assert
+    EXPECT_EQ(first_result, second_result);
+}
+
+
+
+
+
+
