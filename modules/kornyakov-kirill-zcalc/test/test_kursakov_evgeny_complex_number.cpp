2786ff1a39d0000ee117af3879efa10a74b82f59
Evgeny Kursakov <ekursakov@yandex.ru>
2016-03-27 21:39:36 +0300

    Added tests for commutative property

--- modules/kornyakov-kirill-zcalc/test/test_kursakov_evgeny_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_kursakov_evgeny_complex_number.cpp
@@ -16,3 +16,29 @@ TEST(Kursakov_Evgeny_ComplexNumberTest, Multiplication_Of_Complex_Number_And_Con
     ComplexNumber expected_z(13.0, 0);
     EXPECT_EQ(z3, expected_z);
 }
+
+TEST(Kursakov_Evgeny_ComplexNumberTest, Multiplication_Is_Commutative) {
+    // Arrange
+    ComplexNumber z1(26.0, 14.0);
+    ComplexNumber z2(1.0, 7.0);
+
+    // Act
+    ComplexNumber z3 = z1 * z2;
+    ComplexNumber z4 = z2 * z1;
+
+    // Assert
+    EXPECT_EQ(z3, z4);
+}
+
+TEST(Kursakov_Evgeny_ComplexNumberTest, Add_Is_Commutative) {
+    // Arrange
+    ComplexNumber z1(26.0, 14.0);
+    ComplexNumber z2(1.0, 7.0);
+
+    // Act
+    ComplexNumber z3 = z1 + z2;
+    ComplexNumber z4 = z2 + z1;
+
+    // Assert
+    EXPECT_EQ(z3, z4);
+}
