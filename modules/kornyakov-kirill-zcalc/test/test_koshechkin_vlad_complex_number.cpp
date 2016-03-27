334671d4618c17e11fffb08e869cff861f3989f2
Vlad Koshechkin <koshechkin-vlad@yandex.ru>
2016-03-27 21:47:09 +0300

    create 2 tests

--- modules/kornyakov-kirill-zcalc/test/test_koshechkin_vlad_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_koshechkin_vlad_complex_number.cpp
@@ -6,10 +6,12 @@
 #include <gtest/gtest.h>
 
 #include "include/complex_number.h"
+using std::vector;
+using std::string;
 
-TEST(Kornyakov_Kirill_ComplexNumberTest, Can_Create_Zero) {
+TEST(Koshechkin_Vlad_ComplexNumberTest, Can_Create_With_Zero_Imaginary) {
     // Arrange
-    double re = 0.0;
+    double re = 2.0;
     double im = 0.0;
 
     // Act
@@ -17,5 +19,17 @@ TEST(Kornyakov_Kirill_ComplexNumberTest, Can_Create_Zero) {
 
     // Assert
     EXPECT_EQ(re, z.getRe());
-    EXPECT_EQ(im, z.getIm());
+    EXPECT_DOUBLE_EQ(im, z.getIm());
+}
+TEST(Koshechkin_Vlad_ComplexNumberTest, Can_Assignment) {
+    // Arrange
+    ComplexNumber z;
+    ComplexNumber c(1.0, 2.0);
+
+    // Act
+    z=c;
+
+    // Assert
+    EXPECT_DOUBLE_EQ(1.0, z.getRe());
+    EXPECT_DOUBLE_EQ(2.0, z.getIm());
 }
