7189b654fe85d25383d09c479ab43179baa4f0cf
Lenferd <DeysonSH@gmail.com>
2016-03-28 19:23:17 +0300

    Подстраиваемся под Travis

--- modules/kornyakov-kirill-zcalc/test/test_shumihin_sergey_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_shumihin_sergey_complex_number.cpp
@@ -4,8 +4,7 @@
 
 #include "include/complex_number.h"
 
-TEST(Shumihin_Sergey_ComplexNumberTest, multiplication_is_associative)
-{
+TEST(Shumihin_Sergey_ComplexNumberTest, multiplication_is_associative) {
     // Arrange
     ComplexNumber cNum1(2.0, 1.0);
     ComplexNumber cNum2(0.0, 5.0);
@@ -15,8 +14,7 @@ TEST(Shumihin_Sergey_ComplexNumberTest, multiplication_is_associative)
     EXPECT_EQ(cNum1*(cNum2*cNum3), (cNum1*cNum2)*cNum3);
 }
 
-TEST(Shumihin_Sergey_ComplexNumberTest, cant_divided_by_zero)
-{
+TEST(Shumihin_Sergey_ComplexNumberTest, cant_divided_by_zero) {
     // Arrange
     ComplexNumber cNum1(1.0, 2.0);
     ComplexNumber cNum2(0.0, 0.0);
@@ -25,8 +23,7 @@ TEST(Shumihin_Sergey_ComplexNumberTest, cant_divided_by_zero)
     EXPECT_ANY_THROW(cNum1 / cNum2);
 }
 
-TEST(Shumihin_Sergey_ComplexNumberTest, can_assign_to_youself)
-{
+TEST(Shumihin_Sergey_ComplexNumberTest, can_assign_to_youself) {
     // Arrange
     ComplexNumber cNum1(2.3, 1.0);
 
@@ -38,8 +35,7 @@ TEST(Shumihin_Sergey_ComplexNumberTest, can_assign_to_youself)
     EXPECT_EQ(expected_cNum, cNum1);
 }
 
-TEST(Shumihin_Sergey_ComplexNumberTest, can_initialize_using_int)
-{
+TEST(Shumihin_Sergey_ComplexNumberTest, can_initialize_using_int) {
     // Arrange
     ComplexNumber cNum(1, 2);
 
@@ -48,8 +44,7 @@ TEST(Shumihin_Sergey_ComplexNumberTest, can_initialize_using_int)
     EXPECT_EQ(expected_cNum, cNum);
 }
 
-TEST(Shumihin_Sergey_ComplexNumberTest, can_use_multi_oper_in_one_line )
-{
+TEST(Shumihin_Sergey_ComplexNumberTest, can_use_multi_oper_in_one_line) {
     // Arrange
     ComplexNumber cNum1(2, 1);
     ComplexNumber cNum2(6, 0);
@@ -64,8 +59,7 @@ TEST(Shumihin_Sergey_ComplexNumberTest, can_use_multi_oper_in_one_line )
     EXPECT_EQ(expect_cNum, result_cNum);
 }
 
-TEST(Shumihin_Sergey_ComplexNumberTest, square_of_Im_is_equal_to_minus_one)
-{
+TEST(Shumihin_Sergey_ComplexNumberTest, square_of_Im_is_equal_to_minus_one) {
     // Arrange
     ComplexNumber cNum(0.0, 1.0);
 
