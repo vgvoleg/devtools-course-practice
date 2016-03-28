3860aa80d9384ed1358d745a4da3f63f9fa5f5f2
Никита Сироткин < >
2016-03-28 19:50:18 +0300

    fix style error

--- modules/kornyakov-kirill-zcalc/test/test_sirotkin_nikita_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_sirotkin_nikita_complex_number.cpp
@@ -4,75 +4,75 @@
 
 #include "include/complex_number.h"
 
-TEST(Sirotkin_Nikita_ComplexNumberTest, Im_division_by_Im_is_real) {
+TEST(Sirotkin_Nikita_ComplexNumberTest, im_division_by_im_is_real) {
     // Arrange
     double re = 0;
     double im_a = 2;
     double im_b = 1;
-
-    // Act
+    double re_expect = 2;
+    double im_expect = 0;
     ComplexNumber a(re, im_a);
     ComplexNumber b(re, im_b);
+
+    // Act
     ComplexNumber res = a / b;
 
     // Assert
-    double re_expect = 2;
-    double im_expect = 0;
     ASSERT_DOUBLE_EQ(re_expect, res.getRe());
     ASSERT_DOUBLE_EQ(im_expect, res.getIm());
 }
 
-TEST(Sirotkin_Nikita_ComplexNumberTest, Re_mult_on_Im_is_Im) {
+TEST(Sirotkin_Nikita_ComplexNumberTest, re_mult_on_im_is_im) {
     // Arrange
     double re_a = 2;
     double im_a = 0;
     double re_b = 0;
     double im_b = 5;
-
-    // Act
+    double re_expect = 0;
+    double im_expect = 10;
     ComplexNumber a(re_a, im_a);
     ComplexNumber b(re_b, im_b);
+
+    // Act
     ComplexNumber res = a * b;
 
     // Assert
-    double re_expect = 0;
-    double im_expect = 10;
     ASSERT_DOUBLE_EQ(re_expect, res.getRe());
     ASSERT_DOUBLE_EQ(im_expect, res.getIm());
 }
 
-TEST(Sirotkin_Nikita_ComplexNumberTest, Z_add_null_is_Z) {
+TEST(Sirotkin_Nikita_ComplexNumberTest, z_add_null_is_z) {
     // Arrange
     double re = 2;
     double im = 5;
-
-    // Act
+    double re_expect = 2;
+    double im_expect = 5;
     ComplexNumber a(re, im);
     ComplexNumber b(0, 0);
+
+    // Act
     ComplexNumber res = a + b;
 
     // Assert
-    double re_expect = 2;
-    double im_expect = 5;
     ASSERT_DOUBLE_EQ(re_expect, res.getRe());
     ASSERT_DOUBLE_EQ(im_expect, res.getIm());
 }
 
-TEST(Sirotkin_Nikita_ComplexNumberTest, Can_sum_many_times) {
+TEST(Sirotkin_Nikita_ComplexNumberTest, can_sum_many_times) {
     // Arrange
     double re = 2;
     double im = 3;
-    int count = 1000;
-
-    // Act
+    const int count = 1000;
+    double re_expect = 2*count;
+    double im_expect = 3*count;
     ComplexNumber a(re, im);
     ComplexNumber res(0, 0);
+
+    // Act
     for (int i = 0; i < count; ++i)
         res = res + a;
 
     // Assert
-    double re_expect = 2*count;
-    double im_expect = 3*count;
     ASSERT_DOUBLE_EQ(re_expect, res.getRe());
     ASSERT_DOUBLE_EQ(im_expect, res.getIm());
 }
