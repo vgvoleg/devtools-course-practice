9dfecd95dcfc263c6f92f5daeafd3729579e45e1
Asya <6991as@mail.ru>
2016-03-29 22:23:43 +0400

    correct error Arhipycheva

--- modules/kornyakov-kirill-zcalc/test/test_arhipycheva_anastasial_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_arhipycheva_anastasial_complex_number.cpp
@@ -1,16 +1,13 @@
+// Copyright 2016 Arhipycheva Anastasia
 #include <gtest/gtest.h>
-
 #include "include/complex_number.h"
 
 TEST(Arhipycheva_Anastasia_ComplexNumberTest, Can_Create_From_Double) {
-	
   double re = -922337203685;
   double im = 260.45682;
 
-
   ComplexNumber complex(re, im);
 
-  
   EXPECT_DOUBLE_EQ(-922337203685, complex.getRe());
   EXPECT_DOUBLE_EQ(260.45682, complex.getIm());
 }
@@ -18,15 +15,15 @@ TEST(Arhipycheva_Anastasia_ComplexNumberTest, Can_Create_From_Double) {
 TEST(Arhipycheva_Anastasia_ComplexNumberTest, Can_Addition_With_Opposing_Number) {
   int re = 140;
   int im = 444;
+
   ComplexNumber null(0, 0);
   ComplexNumber complex1(re, -im);
   ComplexNumber complex2(-re, im);
-  
+
   EXPECT_EQ(null, complex1 + complex2);
 }
 
 TEST(Arhipycheva_Anastasia_ComplexNumberTest, Can_Comparison_When_Only_Imaginary) {
-  
   int re = 0;
   int im1 = 444;
   int im2 = 666;
@@ -38,11 +35,10 @@ TEST(Arhipycheva_Anastasia_ComplexNumberTest, Can_Comparison_When_Only_Imaginary
 }
 
 TEST(Arhipycheva_Anastasia_ComplexNumberTest, Can_Comparison_When_Only_Real) {
-  
   int re1 = 123;
   int re2 = 12;
   int im = 0;
-  
+
   ComplexNumber complex1(re1, im);
   ComplexNumber complex2(re2, im);
 
@@ -57,14 +53,12 @@ TEST(Arhipycheva_Anastasia_ComplexNumberTest, Can_Comparison_When_Different_Type
 
   ComplexNumber complex1(re1, im1);
   ComplexNumber complex2(re2, im2);
-  
-  EXPECT_EQ(true, complex1 != complex2);
 
+  EXPECT_EQ(true, complex1 != complex2);
 }
 
 
 TEST(Arhipycheva_Anastasia_ComplexNumberTest, Can_Multiplication_When_Null) {
-  
   int re1 = 0;
   int re2 = 546;
   int im1 = 789;
@@ -73,6 +67,6 @@ TEST(Arhipycheva_Anastasia_ComplexNumberTest, Can_Multiplication_When_Null) {
   ComplexNumber null(0, 0);
   ComplexNumber complex1(re1, im1);
   ComplexNumber complex2(re2, im2);
-  
+
   EXPECT_EQ(null, complex1*complex2);
 }
