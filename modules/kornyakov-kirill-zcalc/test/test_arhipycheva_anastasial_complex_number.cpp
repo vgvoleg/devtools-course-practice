50743d1b0e94aff8201d8e4136f7dd955fa36a5f
Asya <6991as@mail.ru>
2016-03-29 22:34:25 +0400

    correct error 2 Arhipycheva

--- modules/kornyakov-kirill-zcalc/test/test_arhipycheva_anastasial_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_arhipycheva_anastasial_complex_number.cpp
@@ -12,7 +12,7 @@ TEST(Arhipycheva_Anastasia_ComplexNumberTest, Can_Create_From_Double) {
   EXPECT_DOUBLE_EQ(260.45682, complex.getIm());
 }
 
-TEST(Arhipycheva_Anastasia_ComplexNumberTest, Can_Addition_With_Opposing_Number) {
+TEST(Arhipycheva_Anastasia_ComplexNumberTest, Addition_With_Opposing_Number) {
   int re = 140;
   int im = 444;
 
@@ -23,7 +23,7 @@ TEST(Arhipycheva_Anastasia_ComplexNumberTest, Can_Addition_With_Opposing_Number)
   EXPECT_EQ(null, complex1 + complex2);
 }
 
-TEST(Arhipycheva_Anastasia_ComplexNumberTest, Can_Comparison_When_Only_Imaginary) {
+TEST(Arhipycheva_Anastasia_ComplexNumberTest, Comparison_When_Only_Imaginary) {
   int re = 0;
   int im1 = 444;
   int im2 = 666;
@@ -34,7 +34,7 @@ TEST(Arhipycheva_Anastasia_ComplexNumberTest, Can_Comparison_When_Only_Imaginary
   EXPECT_EQ(false, complex1 == complex2);
 }
 
-TEST(Arhipycheva_Anastasia_ComplexNumberTest, Can_Comparison_When_Only_Real) {
+TEST(Arhipycheva_Anastasia_ComplexNumberTest, Comparison_When_Only_Real) {
   int re1 = 123;
   int re2 = 12;
   int im = 0;
@@ -45,7 +45,7 @@ TEST(Arhipycheva_Anastasia_ComplexNumberTest, Can_Comparison_When_Only_Real) {
   EXPECT_EQ(true, complex1 != complex2);
 }
 
-TEST(Arhipycheva_Anastasia_ComplexNumberTest, Can_Comparison_When_Different_Types) {
+TEST(Arhipycheva_Anastasia_ComplexNumberTest, Comparison_Different_Types) {
   int re1 = 123;
   double re2 = 12.556;
   double im1 = 13.487;
