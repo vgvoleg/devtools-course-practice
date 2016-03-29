38581d5a73b969da9f4a553fa7662dfb19747cf3
RyabininK4 <ryabinin96@yandex.ru>
2016-03-29 23:19:38 +0300

    Update test_ryabinin_kirill_complex_number.cpp

--- modules/kornyakov-kirill-zcalc/test/test_ryabinin_kirill_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_ryabinin_kirill_complex_number.cpp
@@ -5,76 +5,76 @@
 #include "include/complex_number.h"
 
 TEST(Ryabinin_Kirill_ComplexNumberTest, Can_Add_Complex_Number) {
-// Arrange
-ComplexNumber x(4.0, 3.0);
-ComplexNumber y(5.0, 2.0);
+     // Arrange
+     ComplexNumber x(4.0, 3.0);
+     ComplexNumber y(5.0, 2.0);
 
-// Act
-ComplexNumber z = x + y;
+     // Act
+     ComplexNumber z = x + y;
 
-// Assert
-ComplexNumber add_xy(9.0, 5.0);
-EXPECT_EQ(z, add_xy);
+     // Assert
+     ComplexNumber add_xy(9.0, 5.0);
+     EXPECT_EQ(z, add_xy);
 }
 
 TEST(Ryabinin_Kirill_ComplexNumberTest, Can_Subtraction_Complex_Number) {
-// Arrange
-ComplexNumber x(20.0, 3.0);
-ComplexNumber y(5.0, 6.0);
+     // Arrange
+     ComplexNumber x(20.0, 3.0);
+     ComplexNumber y(5.0, 6.0);
 
-// Act
-ComplexNumber z = x - y;
+     // Act
+     ComplexNumber z = x - y;
 
-// Assert
-ComplexNumber sub_xy(15.0, -3.0);
-EXPECT_EQ(z, sub_xy);
+     // Assert
+     ComplexNumber sub_xy(15.0, -3.0);
+     EXPECT_EQ(z, sub_xy);
 }
 
 TEST(Ryabinin_Kirill_ComplexNumberTest, Can_Multi_Null_Complex_Number) {
-// Arrange
-ComplexNumber x(1.0, 3.0);
-ComplexNumber y(0.0, 0.0);
+     // Arrange
+     ComplexNumber x(1.0, 3.0);
+     ComplexNumber y(0.0, 0.0);
 
-// Act
-ComplexNumber z = x * y;
+     // Act
+     ComplexNumber z = x * y;
 
-// Assert
-EXPECT_EQ(z, y);
+     // Assert
+     EXPECT_EQ(z, y);
 }
 
 TEST(Ryabinin_Kirill_ComplexNumberTest, Can_Share_NULL_Complex_Number) {
-// Arrange
-ComplexNumber x(0.0, 0.0);
-ComplexNumber y(1.0, 3.0);
+     // Arrange
+     ComplexNumber x(0.0, 0.0);
+     ComplexNumber y(1.0, 3.0);
 
-// Act
-ComplexNumber z = x / y;
+     // Act
+     ComplexNumber z = x / y;
 
-// Assert
-EXPECT_EQ(x, z);
+     // Assert
+     EXPECT_EQ(x, z);
 }
 
 TEST(Ryabinin_Kirill_ComplexNumberTest, Can_Folded_Opposie_Complex_Number) {
-// Arrange
-ComplexNumber x(5.0, -3.0);
-ComplexNumber y(-5.0, 3.0);
+     // Arrange
+     ComplexNumber x(5.0, -3.0);
+     ComplexNumber y(-5.0, 3.0);
 
-// Act
-ComplexNumber z = x + y;
+     // Act
+     ComplexNumber z = x + y;
 
-// Assert
-ComplexNumber folded_z(0.0, 0.0);
-EXPECT_EQ(folded_z, z);
+     // Assert
+     ComplexNumber folded_z(0.0, 0.0);
+     EXPECT_EQ(folded_z, z);
 }
 
 TEST(Ryabinin_Kirill_ComplexNumberTest, Can_Compare_Complex_Number) {
-// Arrange
-ComplexNumber x(5.0, 7.0);
-ComplexNumber y(-5.0, 7.0);
+     // Arrange
+     ComplexNumber x(5.0, 7.0);
+     ComplexNumber y(-5.0, 7.0);
 
-// Act
-bool compare_z = x == y;
+    // Act
+     bool compare_z = x == y;
 
-// Assert
-EXPECT_FALSE(compare_z);
+     // Assert
+     EXPECT_FALSE(compare_z);
 }
