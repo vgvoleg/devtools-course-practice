862cfdfe979078f1f8dd612a0bbdc8565e37730a
LeraMarkova <lera960331@hotbox.ru>
2016-03-30 13:12:45 +0300

    updated tests

--- modules/kornyakov-kirill-zcalc/test/test_markova_lera_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_markova_lera_complex_number.cpp
@@ -6,13 +6,13 @@
 
 TEST(Markova_Lera_ComplexNumberTest, Can_Set_Re_and_Im) {
     // Arrange
-	ComplexNumber example(0.0, 0.0);
+    ComplexNumber example(0.0, 0.0);
     // Act
-	example.setIm(15.0);
-	example.setRe(69.0);
+    example.setIm(15.0);
+    example.setRe(69.0);
     // Assert
-	ComplexNumber expectedValue(69.0, 15.0);
-	EXPECT_EQ(example, expectedValue);
+    ComplexNumber expectedValue(69.0, 15.0);
+    EXPECT_EQ(example, expectedValue);
 }
 
 TEST(Markova_Lera_ComplexNumberTest, Can_Get_Big_Sum) {
@@ -33,55 +33,55 @@ TEST(Markova_Lera_ComplexNumberTest, distributivity_saved) {
     // Arrange
     ComplexNumber firstExample(13.0, 103.0);
     ComplexNumber secondExample(610.0, 140.0);
-	ComplexNumber thirdExample(15.0, 11.0);
+    ComplexNumber thirdExample(15.0, 11.0);
 
-    // Act
-	ComplexNumber leftSide = (firstExample + secondExample) * thirdExample;
-	ComplexNumber rightSide = (firstExample * thirdExample) + (secondExample * thirdExample);
-	
-	// Assert
-	EXPECT_EQ(leftSide, rightSide);
+	// Act
+    ComplexNumber leftSide = (firstExample + secondExample) * thirdExample;
+    ComplexNumber rightSide = (firstExample * thirdExample) + (secondExample * thirdExample);
+
+    // Assert
+    EXPECT_EQ(leftSide, rightSide);
 }
 
 TEST(Markova_Lera_ComplexNumberTest, copy_is_work) {
-	// Arrange
-	ComplexNumber firstExample(153.0,17.0);
-	ComplexNumber secondExampleByCopy;
-	double Im;
-	double Re;
+    // Arrange
+    ComplexNumber firstExample(153.0,17.0);
+    ComplexNumber secondExampleByCopy;
+    double Im;
+    double Re;
 
-	// Act
-	secondExampleByCopy = firstExample;
-	Im = firstExample.getIm();
-	Re = firstExample.getRe();
-	ComplexNumber secondExampleByDoubleGet(Re,Im);
-	
-	// Assert
-	EXPECT_EQ(secondExampleByCopy, secondExampleByDoubleGet);
+    // Act
+    secondExampleByCopy = firstExample;
+    Im = firstExample.getIm();
+    Re = firstExample.getRe();
+    ComplexNumber secondExampleByDoubleGet(Re,Im);
+
+    // Assert
+    EXPECT_EQ(secondExampleByCopy, secondExampleByDoubleGet);
 }
 
 TEST(Markova_Lera_ComplexNumberTest, Can_Multyply) {
-	// Arrange
-	ComplexNumber unitExample(1.0, 1.0);
-	const int dividedIntoTwo=100;
-	const int notDividedIntoTwo = 111;
-	ComplexNumber firstExpected;
-	ComplexNumber secondExpected;
+    // Arrange
+    ComplexNumber unitExample(1.0, 1.0);
+    const int dividedIntoTwo=100;
+    const int notDividedIntoTwo = 111;
+    ComplexNumber firstExpected;
+    ComplexNumber secondExpected;
 
-	// Act
-	for (int i = 0; i < dividedIntoTwo; i++)
-	{
-		unitExample=unitExample * unitExample;
-		firstExpected = unitExample;
-	}
-	unitExample.setIm(1.0); // comeback
-	unitExample.setRe(1.0); // comeback
-	for (int i = 0; i < notDividedIntoTwo; i++)
-	{
-		unitExample = unitExample * unitExample;
-		secondExpected = unitExample;
-	}
-	
-	// Assert
-	EXPECT_NE(firstExpected, secondExpected);
+    // Act
+    for (int i = 0; i < dividedIntoTwo; i++)
+    {
+        unitExample=unitExample * unitExample;
+        firstExpected = unitExample;
+    }
+    unitExample.setIm(1.0); // comeback
+    unitExample.setRe(1.0); // comeback
+    for (int i = 0; i < notDividedIntoTwo; i++)
+    {
+        unitExample = unitExample * unitExample;
+        secondExpected = unitExample;
+    }
+
+    // Assert
+    EXPECT_NE(firstExpected, secondExpected);
 }
