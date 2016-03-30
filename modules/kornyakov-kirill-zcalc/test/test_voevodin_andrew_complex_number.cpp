8ac89e98d7dbe50f89a9237c31e3930c717c04a7
Andrew-Voevodin <andrvoevodin@mail.ru>
2016-03-30 12:04:51 +0300

    Updated tests

--- modules/kornyakov-kirill-zcalc/test/test_voevodin_andrew_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_voevodin_andrew_complex_number.cpp
@@ -8,15 +8,15 @@
 
 TEST(Voevodin_Andrew_ComplexNumberTest, Can_Create_From_ComplexNumber) {
     // Arrange
-    ComplexNumber z(12.34, 5);
+    ComplexNumber ñ(12.34, 5);
 
     // Act
-	ComplexNumber z_new(z);
+	ComplexNumber z_new(ñ);
 
 
     // Assert
-    EXPECT_EQ(z_new.getRe(), z.getRe());
-    EXPECT_EQ(z_new.getIm(), z.getIm());
+    EXPECT_EQ(z_new.getRe(), ñ.getRe());
+    EXPECT_EQ(z_new.getIm(), ñ.getIm());
 }
 
 TEST(Voevodin_Andrew_ComplexNumberTest, Triangle_Inequality) {
