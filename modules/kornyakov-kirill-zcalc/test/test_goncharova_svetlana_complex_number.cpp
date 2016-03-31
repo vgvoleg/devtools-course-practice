907cc725ea5139ac3a55420570f4afdb67f824de
SvetlanaGoncharova <emeladris@yandex.ru>
2016-03-31 14:02:51 +0300

    поправки после ревью

--- modules/kornyakov-kirill-zcalc/test/test_goncharova_svetlana_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_goncharova_svetlana_complex_number.cpp
@@ -15,18 +15,18 @@ TEST(Goncharova_Svetlana_ComplexNumberTest, sqr_imaginary_one_is_one) {
   EXPECT_EQ(z_result, z_expected);
 }
 
-TEST(Goncharova_Svetlana_ComplexNumberTest, associativity_test) {
+TEST(Goncharova_Svetlana_ComplexNumberTest, associativity_amount_is_performed) {
   // Arrange
   ComplexNumber z1(1.0, -2.0);
   ComplexNumber z2(0.0, 4.0);
   ComplexNumber z3(-1.0, -2.0);
 
   // Act
-  ComplexNumber z = (z1 + z2) + z3;
+  ComplexNumber z_result = (z1 + z2) + z3;
 
   // Assert
   ComplexNumber z_expected= z1+(z2+z3);
-  EXPECT_EQ(z, z_expected);
+  EXPECT_EQ(z_result, z_expected);
 }
 
 TEST(Goncharova_Svetlana_ComplexNumberTest, sqr_conjugate_numbers) {
@@ -35,35 +35,35 @@ TEST(Goncharova_Svetlana_ComplexNumberTest, sqr_conjugate_numbers) {
   ComplexNumber z2(1.0, 2.0);
 
   // Act
-  ComplexNumber z_rezult = z1+z2;
+  ComplexNumber z_result = z1+z2;
 
   // Assert
   ComplexNumber z_expected(2*z1.getRe(), 0.0);
-  EXPECT_EQ(z_rezult, z_expected);
+  EXPECT_EQ(z_result, z_expected);
 }
 
-TEST(Goncharova_Svetlana_ComplexNumberTest, commutativity_test) {
+TEST(Goncharova_Svetlana_ComplexNumberTest, associativity_multiplication_is performed) {
   // Arrange
   ComplexNumber z1(3.0, 2.0);
   ComplexNumber z2(-9.0, 30.0);
   ComplexNumber z3(0.0, -5.0);
 
   // Act
-  ComplexNumber z = (z1 * z2) * z3;
+  ComplexNumber z_result = (z1 * z2) * z3;
 
   // Assert
   ComplexNumber z_expected = z1 * (z2 * z3);
-  EXPECT_EQ(z, z_expected);
+  EXPECT_EQ(z_result, z_expected);
 }
 
-TEST(Goncharova_Svetlana_ComplexNumberTest, multiplication_test) {
+TEST(Goncharova_Svetlana_ComplexNumberTest, multiplication_through_the_construction_of_the_power) {
   // Arrange
   ComplexNumber z1(3.0, -2.0);
 
   // Act
-  ComplexNumber z = z1*z1*z1;
+  ComplexNumber z_result = z1*z1*z1;
 
   // Assert
   ComplexNumber z_expected(-9.0, -46.0);
-  EXPECT_EQ(z, z_expected);
+  EXPECT_EQ(z_result, z_expected);
 }
