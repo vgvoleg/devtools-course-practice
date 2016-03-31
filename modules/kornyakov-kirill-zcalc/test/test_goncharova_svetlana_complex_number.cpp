0004924f6c6a66db79dca8ea514a015fd1435d06
SvetlanaGoncharova <emeladris@yandex.ru>
2016-03-31 13:16:31 +0300

    изменены пробелы

--- modules/kornyakov-kirill-zcalc/test/test_goncharova_svetlana_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_goncharova_svetlana_complex_number.cpp
@@ -4,52 +4,51 @@
 #include "include/complex_number.h"
 
 TEST(Goncharova_Svetlana_ComplexNumberTest,sqr_imaginary_one_is_one) {
-   //Arrange
-   ComplexNumber z(0.0,1.0);
+   // Arrange
+   ComplexNumber z(0.0, 1.0);
 
-   //Act
+   // Act
    ComplexNumber z_result = z*z;
 
-
-   //Assert
-   ComplexNumber z_expected(-1.0,0.0);
+   // Assert
+   ComplexNumber z_expected(-1.0, 0.0);
    EXPECT_EQ(z_result, z_expected);
 }
 
 TEST(Goncharova_Svetlana_ComplexNumberTest, associativity_test) {
-   //Arrange
+   // Arrange
    ComplexNumber z1(1.0, -2.0);
    ComplexNumber z2(0.0, 4.0);
    ComplexNumber z3(-1.0, -2.0);
 
-   //Act
+   // Act
    ComplexNumber z = (z1 + z2) + z3;
 
-   //Assert
+   // Assert
    ComplexNumber z_expected=z1+(z2+z3);
    EXPECT_EQ(z, z_expected);
 }
 
 TEST(Goncharova_Svetlana_ComplexNumberTest, sqr_conjugate_numbers) {
-   //Arrange
+   // Arrange
    ComplexNumber z1(1.0, -2.0);
    ComplexNumber z2(1.0, 2.0);	
 
-   //Act
+   // Act
    ComplexNumber z_rezult=z1+z2;
 
-   //Assert
+   // Assert
    ComplexNumber z_expected (2*z1.getRe(),0.0);
    EXPECT_EQ(z_rezult, z_expected);
 }
 
 TEST(Goncharova_Svetlana_ComplexNumberTest, commutativity_test) {
-   //Arrange
+   // Arrange
    ComplexNumber z1(3.0, 2.0);
    ComplexNumber z2(-9.0, 30.0);
    ComplexNumber z3(0.0, -5.0);
 
-   //Act
+   // Act
    ComplexNumber z = (z1 * z2) * z3;
 
    // Assert
@@ -61,10 +60,10 @@ TEST(Goncharova_Svetlana_ComplexNumberTest, multiplication_test) {
    // Arrange
    ComplexNumber z1(3.0, -2.0);
 
-   //Act
+   // Act
    ComplexNumber z = z1*z1*z1;
 
-   //Assert
-   ComplexNumber z_expected(-9.0,-46.0);
+   // Assert
+   ComplexNumber z_expected(-9.0, -46.0);
    EXPECT_EQ(z, z_expected);
 }
