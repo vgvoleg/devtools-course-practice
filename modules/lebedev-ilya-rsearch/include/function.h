fc64407046ab8f49056a2d04cb4dc4879116b8e2
lebedevilyag <lebedev.ilya.g@gmail.com>
2016-04-12 22:48:42 +0300

    Случайный поиск минимального значения многомерных функции

---
+++ modules/lebedev-ilya-rsearch/include/function.h
@@ -0,0 +1,24 @@
+// Copyright 2016 Lebedev Ilya
+
+#ifndef MODULES_LEBEDEV_ILYA_RSEARCH_INCLUDE_FUNCTION_H_
+#define MODULES_LEBEDEV_ILYA_RSEARCH_INCLUDE_FUNCTION_H_
+
+#include <math.h>
+#define PI 3.14159265359f
+
+//вычислени целевой функции
+double RastriginFunction(int dimention, double* x)
+{
+  int j;
+  double sum = 0.;
+
+  for (j = 0; j < dimention; j++)
+  {
+    sum += x[j] * x[j] - 10.f * cos(2.f * PI * x[j]) + 10.f;
+  }
+
+  return (sum);
+};
+
+
+#endif //MODULES_LEBEDEV_ILYA_RSEARCH_INCLUDE_FUNCTION_H_
---
+++ modules/lebedev-ilya-rsearch/src/random_search.cxx
@@ -0,0 +1,104 @@
+// Copyright 2016 Lebedev Ilya
+
+#include "include/random_search.h"
+
+#include <stdlib.h>
+#include <string>
+#include <limits>
+
+void RandomSearch::Bounds()
+{
+    for (int i = 0; i < dimention_; i++)
+    {
+        lowerBorder_[i] = -1.0;
+        upperBorder_[i] = 1.0;
+    }
+}
+
+RandomSearch::RandomSearch(tFunction function, int dimention, int iterationCount) : function_(function)
+{
+    lowerBorder_ = 0;
+    upperBorder_ = 0;
+    if (dimention <= 0)
+        throw std::string("Not permissible dimension");
+    if (iterationCount < 1)
+        throw std::string("Invalid number of iterations");
+    dimention_ = dimention;
+    iterationCount_ = iterationCount;
+
+    lowerBorder_ = new double[dimention_];
+    upperBorder_ = new double[dimention_];    
+}
+RandomSearch::~RandomSearch()
+{
+    if (lowerBorder_ != 0)
+    {
+        delete[] lowerBorder_;
+        lowerBorder_ = 0;
+    }
+    if (upperBorder_ != 0)
+    {
+        delete[] upperBorder_;
+        upperBorder_ = 0;
+    }
+}
+
+double RandomSearch::FindMinimum(double* globalMinimum)
+{
+
+    Bounds();
+
+    double* x = new double[dimention_];
+    double* d = new double[dimention_];
+    double val = 0;
+    double minimumValue = std::numeric_limits<double>::max();
+
+    for (int j = 0; j < dimention_; j++)
+        d[j] = upperBorder_[j] - lowerBorder_[j];     
+
+    for (int i = 0; i < iterationCount_; i++)
+    {
+        for (int j = 0; j < dimention_; j++)
+            x[j] = (((float)rand()) / RAND_MAX) * d[j] + lowerBorder_[j];
+
+        val = function_(dimention_, x);
+        if (minimumValue > val)
+        {
+            minimumValue = val;
+            for (int o = 0; o < dimention_; o++)
+                globalMinimum[o] = x[o];
+        }
+    }
+    return minimumValue;
+}
+
+void RandomSearch::SetDimention(int dimention)
+{
+    if (dimention <= 0)
+        throw std::string("Not permissible dimension");
+
+    dimention_ = dimention;
+
+    if (lowerBorder_ != 0)
+        delete[] lowerBorder_;
+    if (upperBorder_ != 0)
+        delete[] upperBorder_;
+
+    lowerBorder_ = new double[dimention_];
+    upperBorder_ = new double[dimention_];
+}
+
+int RandomSearch::GetDimention()
+{
+    return dimention_;
+}
+
+void RandomSearch::SetIterationCount(int iterationCount)
+{
+    iterationCount_ = iterationCount;
+}
+
+int RandomSearch::GetIterationCount()
+{
+    return iterationCount_;
+}
