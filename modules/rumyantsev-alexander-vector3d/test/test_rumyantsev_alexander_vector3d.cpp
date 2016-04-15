338fa176cbe6d5b58f40c449b148f1ed8b0ab04b
cetygamer <cetygamer@gmail.com>
2016-04-15 17:28:24 +0300

    Added some tests for constructors, getters and setters

--- modules/rumyantsev-alexander-vector3d/test/test_rumyantsev_alexander_vector3d.cpp
+++ modules/rumyantsev-alexander-vector3d/test/test_rumyantsev_alexander_vector3d.cpp
@@ -3,3 +3,64 @@
 #include <gtest/gtest.h>
 
 #include "include/vector3d.h"
+
+TEST(Rumyantsev_Alexander_Vector3DTest, Can_Create_Default_Vector) {
+  // Act
+  Vector3D v;
+
+  // Assert
+  EXPECT_DOUBLE_EQ(0.0, v.getX());
+  EXPECT_DOUBLE_EQ(0.0, v.getY());
+  EXPECT_DOUBLE_EQ(0.0, v.getZ());
+}
+
+TEST(Rumyantsev_Alexander_Vector3DTest, Can_Create_With_XYZ) {
+  // Arrange
+  double x = -10.0;
+  double y = 26.0;
+  double z = 0.333;
+
+  // Act
+  Vector3D v(x, y, z);
+
+  // Assert
+  EXPECT_DOUBLE_EQ(x, v.getX());
+  EXPECT_DOUBLE_EQ(y, v.getY());
+  EXPECT_DOUBLE_EQ(z, v.getZ());
+}
+
+TEST(Rumyantsev_Alexander_Vector3DTest, Can_Set_X) {
+  // Arrange
+  Vector3D v;
+  double x = 21.0;
+
+  // Act
+  v.setX(x);
+
+  // Assert
+  ASSERT_DOUBLE_EQ(x, v.getX());
+}
+
+TEST(Rumyantsev_Alexander_Vector3DTest, Can_Set_Y) {
+  // Arrange
+  Vector3D v;
+  double y = -13.2;
+
+  // Act
+  v.setY(y);
+
+  // Assert
+  ASSERT_DOUBLE_EQ(y, v.getY());
+}
+
+TEST(Rumyantsev_Alexander_Vector3DTest, Can_Set_Z) {
+  // Arrange
+  Vector3D v;
+  double z = 0.256;
+
+  // Act
+  v.setZ(z);
+
+  // Assert
+  ASSERT_DOUBLE_EQ(z, v.getZ());
+}
