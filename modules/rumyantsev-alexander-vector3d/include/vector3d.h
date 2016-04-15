1d6f89d51ffbd7f090e9e6b7f4fdbb8e41f84d16
cetygamer <cetygamer@gmail.com>
2016-04-15 10:04:18 +0300

    Added interface for Vector3D class

--- modules/rumyantsev-alexander-vector3d/include/vector3d.h
+++ modules/rumyantsev-alexander-vector3d/include/vector3d.h
@@ -5,8 +5,38 @@
 
 class Vector3D {
  public:
+  Vector3D();
+  Vector3D(const double x, const double y, const double z);
+  Vector3D(const Vector3D& v);
+
+  Vector3D& operator=(const Vector3D& v);
+
+  double getX() const;
+  double getY() const;
+  double getZ() const;
+  void setX(const double x);
+  void setY(const double y);
+  void setZ(const double z);
+
+  Vector3D operator + (const Vector3D& v) const;
+  Vector3D operator - (const Vector3D& v) const;
+  Vector3D operator * (const double k);
+  friend Vector3D operator * (const double k, const Vector3D& v) const;
+  Vector3D operator / (const double k) const;
+
+  bool operator == (const Vector3D& v) const;
+  bool operator != (const Vector3D& v) const;
+
+  double magnitude() const;
+  void normalize();
+  double dot(const Vector3D& v) const;
+  Vector3D cross(const Vector3D& v) const;
 
  private:
+  double x_;
+  double y_;
+  double z_;
+  bool isNullVector(const Vector3D& v) const;
 };
 
 #endif  // MODULES_RUMYANTSEV_ALEXANDER_VECTOR3D_INCLUDE_VECTOR3D_H_
