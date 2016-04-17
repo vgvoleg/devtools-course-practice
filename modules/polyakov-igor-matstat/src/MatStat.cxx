4784d475a09d01afdc018054c2c87b23c331ab69
Polyakov Igor <polykovio@mail.ru>
2016-04-17 12:58:28 +0300

    Some changes in style of code .cxx file for Travis

--- modules/polyakov-igor-matstat/src/MatStat.cxx
+++ modules/polyakov-igor-matstat/src/MatStat.cxx
@@ -7,104 +7,79 @@
 
 
 bool Sample::IsSummOfProbabilitiesEqualUnity(std::vector<double> _prob) {
-
     double sum_of_probabilities = 0.0;
 
-    for (int i = 0; i < _prob.size(); i++)
-    {
+    for (int i = 0; i < _prob.size(); i++) {
         sum_of_probabilities += _prob[i];
     }
 
-    if (abs(1.0 - sum_of_probabilities) > SAMPLE_EPSILON)
-    {
+    if (abs(1.0 - sum_of_probabilities) > SAMPLE_EPSILON) {
         return false;
     }
 
     return true;
-
 }
 
 bool Sample::AreProbabilitiesCorrect(std::vector<double> _probabilities) {
-
-    for (int i = 0; i < _probabilities.size(); i++)
-    {
-        if (_probabilities[i] <= 0.0 || _probabilities[i] > 1.0)
-        {
+    for (int i = 0; i < _probabilities.size(); i++) {
+        if (_probabilities[i] <= 0.0 || _probabilities[i] > 1.0) {
             return false;
         }
     }
 
-    if (!IsSummOfProbabilitiesEqualUnity(_probabilities))
-    {
+    if (!IsSummOfProbabilitiesEqualUnity(_probabilities)) {
         return false;
     }
 
     return true;
-
 }
 
 bool Sample::IsSampleCorrect(std::vector<double> _sample) {
-
-    for (int i = 0; i < _sample.size() - 1; i++)
-    {
-        for (int j = i + 1; j < _sample.size(); j++)
-        {
-            if (_sample[i] == _sample[j])
-            {
+    for (int i = 0; i < _sample.size() - 1; i++) {
+        for (int j = i + 1; j < _sample.size(); j++) {
+            if (_sample[i] == _sample[j]) {
                 return false;
             }
         }
     }
 
     return true;
-
 }
 
 Sample::Sample(std::vector<double> _sample,
-               std::vector<double> _probabilities) {
-
-    if (_sample.size() != _probabilities.size())
-    {
+    std::vector<double> _probabilities) {
+    if (_sample.size() != _probabilities.size()) {
         throw IncorrectSizesOfSampleAndProbabilities();
     }
-    else
-    {
+    else {
         sample_size = _sample.size();
     }
 
-    if (!AreProbabilitiesCorrect(_probabilities))
-    {
+    if (!AreProbabilitiesCorrect(_probabilities)) {
         throw IncorrectProbabilities();
     }
-    else
-    {
+    else {
         probabilities = _probabilities;
     }
 
-    if (!IsSampleCorrect(_sample))
-    {
+    if (!IsSampleCorrect(_sample)) {
         throw IncorrectSample();
     }
-    else
-    {
+    else {
         sample = _sample;
     }
-
 }
 
-Sample::~Sample(){
+Sample::~Sample() {
 }
 
 Sample::Sample(const Sample& S) {
-
     sample_size = S.sample_size;
     sample = S.sample;
     probabilities = S.probabilities;
-
 }
 
 Sample& Sample::operator=(const Sample& S) {
-
     sample_size = S.sample_size;
     sample = S.sample;
     probabilities = S.probabilities;
@@ -113,76 +88,58 @@ Sample& Sample::operator=(const Sample& S) {
 }
 
 bool Sample::operator==(const Sample& S) const {
-
-    if (sample_size < S.sample_size)
-    {
+    if (sample_size < S.sample_size) {
         return false;
     }
 
-    for (int i = 0; i < sample_size; i++)
-    {
-        if (sample[i] != S.sample[i] || probabilities[i] != S.probabilities[i])
-        {
+    for (int i = 0; i < sample_size; i++) {
+        if (sample[i] != S.sample[i] ||
+            probabilities[i] != S.probabilities[i]) {
             return false;
         }
     }
 
     return true;
-
 }
 
 int Sample::GetSampleSize() {
-
     return sample_size;
-
 }
 
 std::vector<double> Sample::GetSample() {
-
     return sample;
-
 }
 
 std::vector<double> Sample::GetProbabilities() {
-
     return probabilities;
-
 }
 
 double Sample::CalcMathematicalExpectation() {
-
     double mathematical_expectation = 0.0;
 
-    for (int i = 0; i < sample_size; i++)
-    {
+    for (int i = 0; i < sample_size; i++) {
         mathematical_expectation += sample[i] * probabilities[i];
     }
 
     return mathematical_expectation;
-
 }
 
 double Sample::CalcMoment(double relative_point,
                           int exponent) {
-
     double moment = 0.0;
 
-    for (int i = 0; i < sample_size; i++)
-    {
+    for (int i = 0; i < sample_size; i++) {
         double power = pow((sample[i] - relative_point), exponent);
         moment += power * probabilities[i];
     }
 
     return moment;
-
 }
 
 double Sample::CalcElementaryMoment(int exponent) {
-
     double elementary_moment = 0.0;
 
-    for (int i = 0; i < sample_size; i++)
-    {
+    for (int i = 0; i < sample_size; i++) {
         double power = pow(sample[i], exponent);
         elementary_moment += power * probabilities[i];
     }
@@ -194,33 +151,26 @@ double Sample::CalcDispersion() {
     double dispersion = 0.0;
     double mathematical_expectation = CalcMathematicalExpectation();
 
-    for (int i = 0; i < sample_size; i++)
-    {
+    for (int i = 0; i < sample_size; i++) {
         double power = pow(sample[i] - mathematical_expectation, 2.0);
         dispersion += power * probabilities[i];
     }
 
     return dispersion;
-
 }
 
 double Sample::CalcAverageQuadraticDeviation() {
-
     return sqrt(CalcDispersion());
-
 }
 
 double Sample::CalcCentralMoment(int exponent) {
-
     double central_moment = 0.0;
     double mathematical_expectation = CalcMathematicalExpectation();
 
-    for (int i = 0; i < sample_size; i++)
-    {
+    for (int i = 0; i < sample_size; i++) {
         double power = pow(sample[i] - mathematical_expectation, exponent);
         central_moment += power * probabilities[i];
     }
 
     return central_moment;
-
 }
