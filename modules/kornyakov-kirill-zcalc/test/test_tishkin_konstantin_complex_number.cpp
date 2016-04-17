49aa587d657d25cb419bd583dd52edfb58cc6aae
Konstantin Tishkin <1995kostia@mail.ru>
2016-04-17 22:50:19 +0300

    Updated tests 4

--- modules/kornyakov-kirill-zcalc/test/test_tishkin_konstantin_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_tishkin_konstantin_complex_number.cpp
@@ -4,6 +4,8 @@
 
 #include "include/complex_number.h"
 
+const double rez = std::numeric_limits<double>::epsilon();
+
 TEST(Tishkin_Kostya_ComplexNumberTest, Can_create_twin) {
     // Arrange
     ComplexNumber a(2.0, 5.0);
@@ -21,32 +23,24 @@ TEST(Tishkin_Kostya_ComplexNumberTest, Can_Copy_Num) {
     ComplexNumber z(2.0, 5.0);
 
     // Act
-    ComplexNumber c = z;
+    ComplexNumber c(z);
 
     // Assert
     EXPECT_EQ(c, z);
 }
 
-TEST(Tishkin_Kostya_ComplexNumberTest, Can_getRE) {
+TEST(Tishkin_Kostya_ComplexNumberTest, Can_getRE_getIM) {
     // Arrange
     double d = 2.0;
+	double h = 5.0;
     // Act
     ComplexNumber a(2.0, 5.0);
 
     // Assert
-    EXPECT_EQ(a.getRe(), d);
+    EXPECT_NEAR(d, a.getRe(), rez);
+    EXPECT_NEAR(h, a.getIm(), rez);
 }
 
-TEST(Tishkin_Kostya_ComplexNumberTest, Can_getIM) {
-    // Arrange
-    double c = 5.0;
-
-    // Act
-    ComplexNumber a(2.0, 5.0);
-
-    // Assert
-    EXPECT_EQ(a.getIm(), c);
-}
 TEST(Tishkin_Kostya_ComplexNumberTest, Can_setIM) {
     // Arrange
     ComplexNumber a(2.0, 5.0);
@@ -57,4 +51,28 @@ TEST(Tishkin_Kostya_ComplexNumberTest, Can_setIM) {
 
     // Assert
     EXPECT_NE(a, b);
+}
+
+TEST(Tishkin_Kostya_ComplexNumberTest, Can_Multiply_One) {
+	// Arrange
+    ComplexNumber a(2.0, 5.0);
+    ComplexNumber b(1.0, 0.0);
+
+	// Act
+    ComplexNumber c = a * b;
+
+	// Assert
+    EXPECT_EQ(c, a);
+}
+
+TEST(Tishkin_Kostya_ComplexNumberTest, Can_Div_One) {
+	// Arrange
+    ComplexNumber a(2.0, 5.0);
+    ComplexNumber b(1.0, 0.0);
+
+	// Act
+    ComplexNumber c = a / b;
+
+	// Assert
+    EXPECT_EQ(c, a);
 }
