2f5b49eab09666ed13e72a3be0dc4a97d8c834f5
Konstantin Tishkin <1995kostia@mail.ru>
2016-04-18 09:10:28 +0300

    Updated tests 4

--- modules/kornyakov-kirill-zcalc/test/test_tishkin_konstantin_complex_number.cpp
+++ modules/kornyakov-kirill-zcalc/test/test_tishkin_konstantin_complex_number.cpp
@@ -1,9 +1,10 @@
 // Copyright 2016 Tishkin Konstantin
 
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
-#include "include/complex_number.h"
-const double rez = 1.0;
+#include <include/complex_number.h>
+
+const double rez = 10.0;
 
 TEST(Tishkin_Kostya_ComplexNumberTest, Can_create_twin) {
     // Arrange
@@ -19,13 +20,13 @@ TEST(Tishkin_Kostya_ComplexNumberTest, Can_create_twin) {
 
 TEST(Tishkin_Kostya_ComplexNumberTest, Can_Copy_Num) {
     // Arrange
-    ComplexNumber z(2.0, 5.0);
+    ComplexNumber a(2.0, 5.0);
 
     // Act
-    ComplexNumber c(z);
+    ComplexNumber b(a);
 
     // Assert
-    EXPECT_EQ(c, z);
+    EXPECT_EQ(b, a);
 }
 
 TEST(Tishkin_Kostya_ComplexNumberTest, Can_getRE_getIM) {
