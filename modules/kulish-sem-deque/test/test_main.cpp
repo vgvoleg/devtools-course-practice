3ba0fcf3d8b6c1e6dffe5f8cbd93f54a261a7190
Семён Кулиш < >
2016-04-22 16:01:21 +0400

    Its over?

--- modules/kulish-sem-deque/test/test_main.cpp
+++ modules/kulish-sem-deque/test/test_main.cpp
@@ -2,10 +2,7 @@
 
 #include <gtest/gtest.h>
 
-using namespace std;
-
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
