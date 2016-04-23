df2d8774458f31591f7f66f966f199a615ebf7a5
ownikss <sirotkin.n.s@gmail.com>
2016-04-23 15:36:00 +0300

    Fix style

--- modules/sirotkin-nikita-finance_notepad/include/list.h
+++ modules/sirotkin-nikita-finance_notepad/include/list.h
@@ -33,45 +33,40 @@ public:
 
 //node
 template<class ELEMENTS_TYPE>
-node<ELEMENTS_TYPE>::node(ELEMENTS_TYPE _value): value(_value), next(nullptr)
-{
+node<ELEMENTS_TYPE>::node(ELEMENTS_TYPE _value): value(_value), next(nullptr){
 }
 
 template<class ELEMENTS_TYPE>
-ELEMENTS_TYPE node<ELEMENTS_TYPE>::getValue()
-{
+ELEMENTS_TYPE node<ELEMENTS_TYPE>::getValue(){
 	return value;
 }
+
 template<class ELEMENTS_TYPE>
-node<ELEMENTS_TYPE>* node<ELEMENTS_TYPE>::getNext()
-{
+node<ELEMENTS_TYPE>* node<ELEMENTS_TYPE>::getNext(){
 	return next;
 }
+
 template<class ELEMENTS_TYPE>
-void node<ELEMENTS_TYPE>::setNext(node<ELEMENTS_TYPE>* _next)
-{
+void node<ELEMENTS_TYPE>::setNext(node<ELEMENTS_TYPE>* _next){
 	next = _next;
 }
+
 template<class ELEMENTS_TYPE>
-void node<ELEMENTS_TYPE>::setValue(ELEMENTS_TYPE _value)
-{
+void node<ELEMENTS_TYPE>::setValue(ELEMENTS_TYPE _value){
 	value = _value;
 }
 
 //list
 template<class ELEMENTS_TYPE>
-list<ELEMENTS_TYPE>::list(): head(nullptr), last(nullptr), current(nullptr)
-{
+list<ELEMENTS_TYPE>::list(): head(nullptr), last(nullptr), current(nullptr){
 }
 
 template<class ELEMENTS_TYPE>
-list<ELEMENTS_TYPE>::~list()
-{
+list<ELEMENTS_TYPE>::~list(){
 	node<ELEMENTS_TYPE>* deleting;
 	node<ELEMENTS_TYPE>* next_deleting;
 	deleting = head;
-	while (deleting != nullptr)
-	{
+	while (deleting != nullptr){
 		next_deleting = deleting->getNext();
 		delete deleting;
 		deleting = next_deleting;
@@ -79,79 +74,60 @@ list<ELEMENTS_TYPE>::~list()
 }
 
 template<class ELEMENTS_TYPE>
-void list<ELEMENTS_TYPE>::addElement(ELEMENTS_TYPE value)
-{
+void list<ELEMENTS_TYPE>::addElement(ELEMENTS_TYPE value){
 	node<ELEMENTS_TYPE>* added = new node<ELEMENTS_TYPE>(value);
-	if (last == nullptr)
-	{
+	if (last == nullptr){
 		head = added;
 		current = head;
-	}
-	else
+	}	else
 		last->setNext(added);
 	last = added;
 }
 
 template<class ELEMENTS_TYPE>
-bool list<ELEMENTS_TYPE>::operator++()
-{
-	if (current != nullptr)
-	{
+bool list<ELEMENTS_TYPE>::operator++(){
+	if (current != nullptr){
 		current = current->getNext();
-		if (current != nullptr)
-			return true;
-		else
-		{
+		if (current != nullptr)	return true;
+		else {
 			current = head;
 			return false;
 		}
-	}
-	else
-		return false;
+	}	else return false;
 }
 
 template<class ELEMENTS_TYPE>
-ELEMENTS_TYPE list<ELEMENTS_TYPE>::getValue() const
-{
+ELEMENTS_TYPE list<ELEMENTS_TYPE>::getValue() const{
 	return current->getValue();
 }
 
 template<class ELEMENTS_TYPE>
-void list<ELEMENTS_TYPE>::reset()
-{
+void list<ELEMENTS_TYPE>::reset(){
 	current = head;
 }
 
 template<class ELEMENTS_TYPE>
-bool list<ELEMENTS_TYPE>::isEmpty()
-{
+bool list<ELEMENTS_TYPE>::isEmpty(){
 	return (head == nullptr);
 }
 
 template<class ELEMENTS_TYPE>
-void list<ELEMENTS_TYPE>::sort()
-{
-	if (head != nullptr)
-	{
+void list<ELEMENTS_TYPE>::sort(){
+	if (head != nullptr){
 		node<ELEMENTS_TYPE> *first = head;
 		node<ELEMENTS_TYPE> *second;
 		ELEMENTS_TYPE for_swap = head->getValue();
-		{
-			while (first != nullptr)
-			{
-				second = first->getNext();
-				while (second != nullptr)
-				{
-					if (first->getValue() > second->getValue())
-					{
-						for_swap = second->getValue();
-						second->setValue(first->getValue());
-						first->setValue(for_swap);
-					}
-					second = second->getNext();
+		while (first != nullptr){
+			second = first->getNext();
+			while (second != nullptr){
+				if (first->getValue() > second->getValue()){
+					for_swap = second->getValue();
+					second->setValue(first->getValue());
+					first->setValue(for_swap);
 				}
-				first = first->getNext();
+				second = second->getNext();
 			}
+			first = first->getNext();
 		}
 	}
 }
--- modules/sirotkin-nikita-finance_notepad/src/finance_notepad.cxx
+++ modules/sirotkin-nikita-finance_notepad/src/finance_notepad.cxx
@@ -6,33 +6,30 @@ using namespace std;
 #define WRONG_DATE date(1, months::JANUARY, 2000)
 
 template <class TYPE>
-string to_string(TYPE value)
-{
+string to_string(TYPE value){
     ostringstream oss;
     oss<< value;
     return oss.str();
 }
 
 //date
-date::date(char _number, months _month, int _year)
-{
+date::date(char _number, months _month, int _year){
 	if (_number >31 || _year < 2000 || _year > 2100)
 		*this = WRONG_DATE;
-	else
-	{
+	else	{
 		number = _number;
 		month = _month;
 		year = _year;
 	}
 }
-string date::toString() const
-{
+
+string date::toString() const{
 	string result;
 	result = to_string<int>(number) + "."+ to_string<int>(int(month)+1)+"."+ to_string<int>(year);
 	return result;
 }
-bool date::operator>(date other_date) const
-{
+
+bool date::operator>(date other_date) const{
 	if (year > other_date.year)
 		return true;
 	else
@@ -50,14 +47,13 @@ bool date::operator>(date other_date) const
 					else
 						return false;
 }
-bool date::operator<(date other_date) const
-{
+
+bool date::operator<(date other_date) const{
 	return !((*this)>other_date);
 }
 
 //category_table
-category_table::category_table()
-{
+category_table::category_table(){
 	table[0] = "Debt";
 	table[1] = "Stipend";
 	table[2] = "Salary";
@@ -68,34 +64,33 @@ category_table::category_table()
 	table[7] = "Taxes";
 	table[8] = "Other";
 }
-int category_table::getIdOf(string name)
-{
+
+int category_table::getIdOf(string name){
 	for (int i = 0; i < 8; ++i)
 		if (name == table[i])
 			return i;
 	return 8;
 }
-string category_table::getNameOf(int id)
-{
+
+string category_table::getNameOf(int id){
 	return table[id];
 }
 
 //note
 note::note(date _notes_date, float _sum, int _categories_id, category_table* _table_of_categories, string _comment) :
-  notes_date(_notes_date), sum(_sum), categories_id(_categories_id), table(_table_of_categories), comment(_comment)
-{
+  notes_date(_notes_date), sum(_sum), categories_id(_categories_id), table(_table_of_categories), comment(_comment){
 
 }
-int note::getCategoriesId()
-{
+
+int note::getCategoriesId(){
 	return categories_id;
 }
-float note::getSum()
-{
+
+float note::getSum(){
 	return sum;
 }
-string note::toString() const
-{
+
+string note::toString() const{
 	string result;
 	result = notes_date.toString() + "\nSum: " + to_string<float>(sum)
 		+ "\nCategory: " + table->getNameOf(categories_id)
@@ -103,35 +98,32 @@ string note::toString() const
 	return result;
 }
 
-bool note::operator>(const note &right) const
-{
+bool note::operator>(const note &right) const{
 	return (notes_date > right.notes_date);
 }
-bool note::operator<(const note &right) const
-{
+
+bool note::operator<(const note &right) const{
 	return (notes_date < right.notes_date);
 }
 
 //notepad
-notepad::notepad(float _pouch): pouch(_pouch)
-{
+notepad::notepad(float _pouch): pouch(_pouch){
 }
-void notepad::addNote(date _notes_date, float _sum, string _categoriy, string _comment)
-{
+void notepad::addNote(date _notes_date, float _sum, string _categoriy, string _comment){
 	notes.addElement(note(_notes_date, _sum, table_of_categories.getIdOf(_categoriy), &table_of_categories, _comment));
 	pouch += _sum;
 }
-float notepad::getPouch() const
-{
+
+float notepad::getPouch() const{
 	return pouch;
 }
-float notepad::getPotentialPouch()
-{
+
+float notepad::getPotentialPouch(){
 	float result = pouch - sumFromCategory("Debt");
 	return result;
 }
-string notepad::notesFromCategoryToString(string category)
-{
+
+string notepad::notesFromCategoryToString(string category){
 	string result;
 	notes.reset();
 	int category_id = table_of_categories.getIdOf(category);
@@ -144,8 +136,8 @@ string notepad::notesFromCategoryToString(string category)
 	}
 	return result;
 }
-float notepad::sumFromCategory(string category)
-{
+
+float notepad::sumFromCategory(string category){
 	float result = 0.f;
 	notes.reset();
 	int category_id = table_of_categories.getIdOf(category);
@@ -158,8 +150,8 @@ float notepad::sumFromCategory(string category)
 	}
 	return result;
 }
-string notepad::toString()
-{
+
+string notepad::toString(){
 	string result;
 	notes.reset();
 	bool flag = !(notes.isEmpty());
@@ -170,8 +162,8 @@ string notepad::toString()
 	}
 	return result;
 }
-string notepad::stringGroupedByCategories()
-{
+
+string notepad::stringGroupedByCategories(){
 	string result;
 	bool flag;
 	for (int category_id = 0; category_id < 9; ++category_id)
@@ -188,15 +180,14 @@ string notepad::stringGroupedByCategories()
 	return result;
 }
 
-void notepad::sortByDate()
-{
+void notepad::sortByDate(){
 	notes.sort();
 }
-bool notepad::operator++()
-{
+
+bool notepad::operator++(){
 	return ++notes;
 }
-note notepad::getCurrentNote()
-{
+
+note notepad::getCurrentNote(){
 	return notes.getValue();
 }
