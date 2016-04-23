33daa6ff328dd8b15143bd5dfdd98243f709d025
Семён Кулиш < >
2016-04-23 21:53:13 +0400

    test_main fix

--- modules/kulish-sem-deque/test/test_main.cpp
+++ modules/kulish-sem-deque/test/test_main.cpp
@@ -1,10 +1,7 @@
 // Copyright Kulish_Sem 2016
 
 #include <gtest/gtest.h>
-#include "TDeque.hpp"
-#include <iostream>
-using std::cout;
-using std::endl;
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
