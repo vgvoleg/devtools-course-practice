d433c62b431d7990b202ac97c51c932f6b10a953
Maria Pronina <maria.pronina@outlook.com>
2016-04-25 10:37:10 +0300

    changed order of public and private members

--- modules/pronina-maria-romanconvert/include/RomanConverter.h
+++ modules/pronina-maria-romanconvert/include/RomanConverter.h
@@ -3,17 +3,16 @@
 #define MODULES_PRONINA_MARIA_ROMANCONVERT_INCLUDE_ROMANCONVERTER_H_
 #include <vector>
 #include <string>
-class RomanConverter {
-    static const std::vector<int> values;
-    static const std::vector<std::string> symbols;
-    static const int maxValue;
+class RomanConverter {    
  public:
     static bool CheckArabicNumber(const int& arabic);
     static bool CheckRomanNumber(const std::string& roman);
     static int ConvertRomanToArabic(const std::string& roman);
     static std::string ConvertArabicToRoman(const int& arabic);
-
  private:
+    static const std::vector<int> values;
+    static const std::vector<std::string> symbols;
+    static const int maxValue;
     static std::vector<std::string> ParseRoman(const std::string& roman);
     static bool CheckNumeralsRepeats
                 (const std::vector<std::string>& parsed_roman);
