477da8a296f51cdf7c5f07f84bf0aec63a3a2f32
Asya <6991as@mail.ru>
2016-04-25 11:47:22 +0400

    1.1

---
+++ modules/arhipycheva-anastasia-elasticity/include/TElasticity.h
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class TElasticity
+{
+public:
+	void ElasticityByPricePoint(int Q1, int Q0, float P1, float P0);
+	void ElasticityByPriceArc(int Q1, int Q0, float P1, float P0);
+	void ElasticityByIncome(int Q1, int Q0, float I1, float I0);
+	void ElasticityCross(int QA1, int QA0, float PB1, float PB0);
+
+	int ElasticityByPricePointDo(int Q1, int Q0, float P1, float P0);
+	int ElasticityByPriceArcDo(int Q1, int Q0, float P1, float P0);
+	int ElasticityByIncomeDo(int Q1, int Q0, float I1, float I0);
+	int ElasticityCrossDo(int QA1, int QA0, float PB1, float PB0);
+};
