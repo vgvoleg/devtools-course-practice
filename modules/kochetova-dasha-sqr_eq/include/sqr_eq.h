3e1e90d7a987dee7b32f215edec79d072d66a6aa
Dasha Kochetova <ko4.dasha@yandex.ru>
2016-04-28 00:05:53 +0300

    попытка раз

---
+++ modules/kochetova-dasha-sqr_eq/include/sqr_eq.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+#include <math.h>
+#include <set>
+#include <complex>
+#include <array>
+
+using namespace std;
+
+typedef double DType;
+
+class Equation
+{
+private:
+	DType _a;
+	DType _b;
+	DType _c;
+    double _x1;
+	complex<DType> _cx1;
+	double _x2;
+	complex<DType> _cx2;
+	bool error;
+
+public:
+	Equation();
+    Equation(DType a, DType b, DType c);
+	DType Discrim();
+	set<double> SolveDiscrim();
+	set<double> SolveViet();
+	void SolveComplex();
+	array<double,2> ParabolaTop();
+	DType GetA();
+	DType GetB();
+	DType GetC();
+	DType GetX1();
+	complex<DType> GetCx1();
+	double GetX2();
+	complex<DType> GetCx2();
+	bool GetFlag();
+	void PutParametrs(DType a, DType b, DType c);
+};
---
+++ modules/kochetova-dasha-sqr_eq/src/sqr_eq.cxx
@@ -0,0 +1,171 @@
+#include "include/sqr_eq.h"
+
+Equation::Equation()
+{
+	_a = 1;
+	_b = 1;
+	_c = 1;
+	error = false;
+}
+
+Equation::Equation (DType a, DType b, DType c)
+{
+    if (a != 0)
+    {
+        _a = a;
+		_b = b;
+        _c = c;
+		error = false;
+    }
+    else
+        error = true;
+      //  cout << "Error! You are give not the quadratic equation" << endl;
+}
+
+void Equation::PutParametrs(DType a, DType b, DType c)
+{
+	_a = a;
+	_b = b;
+	_c = c;
+}
+
+DType Equation::Discrim ()
+{
+	DType _dis;
+    _dis = (_b *_b) - (4 * _a * _c);
+    return _dis;
+}
+
+set<double> Equation::SolveDiscrim()
+{
+	set <double> rootset;
+	DType dis = Discrim();
+	if (dis < 0)
+		//      cout << "The equation has no real roots" << endl;
+		SolveComplex();
+    else if (dis > 0)
+    {
+		double di = sqrt(dis);
+        _x1 = (-_b + di) / (2 * _a);
+		rootset.insert(_x1);
+
+        _x2 = (-_b - di) / (2 * _a);
+		rootset.insert(_x2);
+
+        cout << "Discriminant = " << dis << endl;
+		cout << "di =" << di << endl;
+        cout << "x1 = " << _x1 << ", x2 = " << _x2 << endl;
+    }
+    else
+    {
+        _x1 = -_b / (2 * _a);
+		rootset.insert(_x1);
+
+        cout << "Discriminant = " << dis << endl;
+        cout << "x = " << _x1 << endl;
+    }
+	return rootset;
+}
+
+DType Equation::GetA()
+{
+	return _a;
+}
+
+DType Equation::GetB()
+{
+	return _b;
+}
+
+DType Equation::GetC()
+{
+	return _c;
+}
+
+double Equation::GetX1()
+{
+	return _x1;
+}
+
+double Equation::GetX2()
+{
+	return _x2;
+}
+
+bool Equation::GetFlag()
+{
+	return error;
+}
+
+set<double> Equation::SolveViet()
+{
+	set <double> rootset;
+	double sum = fabs(_a) + fabs(_b) + fabs(_c);
+	double p = -(_b / _a);
+	double q = _c / _a;
+
+	for (double i = -sum; i <= sum; i++)
+	{
+		for (double j = i; j <= sum; j++)
+		{
+			if (i + j == p && i*j == q)
+			{
+				_x1 = j;
+				rootset.insert(_x1);
+				_x2 = i;
+				rootset.insert(_x2);
+				break;
+			}
+			else
+				error = true;
+		}
+	}
+	return rootset;
+}
+
+void Equation::SolveComplex()
+{
+	DType dis = Discrim();
+	if (dis < 0)
+	{
+		double _im = sqrt(-dis);
+		complex<DType> x1 (-_b / 2, _im / 2);
+		_cx1 = x1;
+		cout << "x1=" <<real(x1)<<"+"<< imag(x1) << "i" << endl;
+		complex<DType> x2 (-_b / 2, -_im / 2);
+		_cx2 = x2;
+		cout << "x2=" <<real(x2)<< imag(x2) << "i" << endl;
+	}
+	else
+		error = true; 
+}
+
+array<double,2> Equation::ParabolaTop()
+{
+	std::array<double, 2> topcoordinat;
+	if (_a == 0)
+		error = true;
+	else
+	{
+		double x0 = -_b / (2 * _a);
+		topcoordinat[0] = x0;
+		double y0 = (_a*x0*x0) + (_b*x0) + _c;
+		topcoordinat[1] = y0;
+
+		cout << "x0=" << topcoordinat[0];
+		cout << endl;
+		cout << "y0=" << topcoordinat[1];
+		cout << endl;
+	}
+	return topcoordinat;
+}
+
+complex<DType> Equation::GetCx1()
+{
+	return _cx1;
+}
+
+complex<DType> Equation::GetCx2()
+{
+	return _cx2;
+}
