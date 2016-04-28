236739d6249990464bba062e8b33a226cb137790
SvetlanaGoncharova <emeladris@yandex.ru>
2016-04-29 01:51:55 +0300

    поправочки

--- modules/goncharova-svetlana-pars/include/pars.h
+++ modules/goncharova-svetlana-pars/include/pars.h
@@ -1,3 +1,8 @@
+// Copyright 2016 Goncharova Svetlana
+
+#ifndef MODULES_GONCHAROVA_SVETLANA_PARS_INCLUDE_PARSE_H_
+#define MODULES_GONCHAROVA_SVETLANA_PARS_INCLUDE_PARSE_H_
+
 #include <string>
 #include <vector>
 #include <cmath>
@@ -10,16 +15,19 @@ struct Expression {
     string token;
     vector<Expression> args;
 
-    Expression(string token) : token(token) {}
+    explicit Expression(string token) : token(token) {}
     Expression(string token, Expression a) : token(token), args({ a }) {}
-    Expression(string token, Expression a, Expression b) : token(token), args({ a, b }) {}
+    Expression(string token, Expression a, Expression b) :
+        token(token), args({ a, b }) {}
 };
 
 class Parser {
+
 public:
-    Parser(const char* input) : input(input) {}
+    explicit Parser(const char* input) : input(input) {}
     Expression parse();
     static double eval(const Expression& e);
+
 private:
     string parse_token();
     Expression parse_simple_expression();
@@ -27,3 +35,5 @@ private:
     const char* input;
 
 };
+
+#endif  // MODULES_GONCHAROVA_SVETLANA_PARS_INCLUDE_PARSE_H_
