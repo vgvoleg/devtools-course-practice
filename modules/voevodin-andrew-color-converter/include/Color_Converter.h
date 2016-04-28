ddd27b6e92c08b4382d31703ae67e5a84860b0cb
Andrew-Voevodin <andrvoevodin@mail.ru>
2016-04-28 21:46:06 +0300

    Change *.h

--- modules/voevodin-andrew-color-converter/include/Color_Converter.h
+++ modules/voevodin-andrew-color-converter/include/Color_Converter.h
@@ -20,7 +20,7 @@ class IncorrectValueOfRGB :public std::runtime_error {
 class IncorrectValueOfHSV :public std::runtime_error {
  public:
     IncorrectValueOfHSV() :
-        std::runtime_error("Value HSV is not correct, H: 0-1; S: 0-1; V: 0-1") {}
+        std::runtime_error("Value HSV is not correct, H: 0-1; S,V: 0-1") {}
 };
 
 class IncorrectValueOfLAB :public std::runtime_error {
