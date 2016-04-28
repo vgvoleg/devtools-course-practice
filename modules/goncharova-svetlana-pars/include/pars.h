19a0ded9f531d26db5d3fc52ae1f9eb0b54fc010
SvetlanaGoncharova <emeladris@yandex.ru>
2016-04-28 23:43:02 +0300

    жесть

--- modules/goncharova-svetlana-pars/include/pars.h
+++ modules/goncharova-svetlana-pars/include/pars.h
@@ -19,7 +19,6 @@ class Parser {
 public:
     Parser(const char* input) : input(input) {}
     Expression parse();
-    double eval(const Expression& e);
 private:
     string parse_token();
     Expression parse_simple_expression();
