b90fad05d65c598681dae27444e7de2c3cc72d97
Animan2010 <stasm2011@gmail.com>
2016-04-29 14:17:36 +0300

    Added tests, added new methods

---
+++ modules/malkov-stas-path/test/test_main.cxx
@@ -0,0 +1,15 @@
+// Copyright 2016 Malkov Stanislav
+
+#include <iostream>
+#include <gtest/gtest.h>
+
+#include "PathFinder.h"
+
+using namespace std;
+//главная функция
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    RUN_ALL_TESTS();
+    return 0;
+}
