4741de2d656f36543dc8a1be408e64632566032c
Asya <6991as@mail.ru>
2016-04-29 17:27:02 +0400

    1.0

--- modules/arhipycheva-anastasia-elasticity/include/TElasticity.h
+++ modules/arhipycheva-anastasia-elasticity/include/TElasticity.h
@@ -18,7 +18,7 @@ namespace TElasticity {
   int ElasticityByPriceArcDo(int Q1, int Q0, float P1, float P0);
   int ElasticityByIncomeDo(int Q1, int Q0, float I1, float I0);
   int ElasticityCrossDo(int QA1, int QA0, float PB1, float PB0);
-};
+} // namespace TElasticity
 
 
 #endif  // MODULES_ARHIPYCHEVA_ANASTASIA_ELASTICITY_INCLUDE_TELASTICITY_H_
