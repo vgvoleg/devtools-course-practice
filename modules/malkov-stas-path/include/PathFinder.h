4121eee14a99fc56049b13ee2fec2365653b1dad
Animan2010 <stasm2011@gmail.com>
2016-04-29 14:37:46 +0300

    Fix

--- modules/malkov-stas-path/include/PathFinder.h
+++ modules/malkov-stas-path/include/PathFinder.h
@@ -3,30 +3,31 @@
 #ifndef MODULES_MALKOV_STAS_PATH_INCLUDE_PATHFINDER_H_
 #define MODULES_MALKOV_STAS_PATH_INCLUDE_PATHFINDER_H_
 
-#define PATH_NOT_EXISTS -1      // GetDistance возвращает при отсутствии пути к точке
+// GetDistance возвращает при отсутствии пути к точке
+#define PATH_NOT_EXISTS -1
 
 /* Алгоритм Дейкстры.
-С помощью SetGraph передаем указатель на таблицу расстояний между vertice_num вершин, 
-после чего класс обрабатывает и сохраняет данные. */
+С помощью SetGraph передаем указатель на таблицу расстояний между vertice_num
+вершин, после чего класс обрабатывает и сохраняет данные. */
 class PathFinder {
-    bool _IsInitialized = false;                            // Флаг инициализации
-    int ** _Graph;                                          // Граф
-    int _Size;                                              // Размер
-    int _StartVertex;                                       // Вершина, от которой измеряем расстояние
-    int* _Distance;                                         // Минимальные расстояния до вершин
-    bool* _Visited;                                         // Посещенные вершины
-    void UpdateGraph();                                     // Обработка графа
-    void Reset();                                           // Сбрасывает все данные
-    void CheckVetricesWithoutPath();                        // Проверка вершин без пути
+    bool _IsInitialized = false;        // Флаг инициализации
+    int ** _Graph;                      // Граф
+    int _Size;                          // Размер
+    int _StartVertex;                   // Начальная вершина
+    int* _Distance;                     // Минимальные расстояния до вершин
+    bool* _Visited;                     // Посещенные вершины
+    void UpdateGraph();                 // Обработка графа
+    void Reset();                       // Сбрасывает все данные
+    void CheckVetricesWithoutPath();    // Проверка вершин без пути
 public:
     PathFinder();
     PathFinder(int**, int, int = 0);
     ~PathFinder();
-    void SetGraph(int**, int, int = 0);                     // Установка графа и параметров
-    int GetDistance(int);                                   // Получение расстояния
-    int* GetDistances();                                    // Получение всех расстояний
-    void SetStartVertex(int);                               // Установка начальной вершины
-    void SetPath(int, int, int);                            // Установка пути между вершинами
+    void SetGraph(int**, int, int = 0);   // Установка графа и параметров
+    int GetDistance(int);                 // Получение расстояния
+    int* GetDistances();                  // Получение всех расстояний
+    void SetStartVertex(int);             // Установка начальной вершины
+    void SetPath(int, int, int);          // Установка пути между вершинами
 };
 
-#endif
+#endif  // MODULES_MALKOV_STAS_PATH_INCLUDE_PATHFINDER_H_
