bc2c9a19665e4da41fdbe0d62c0717efd04d1223
Baydina O <ol4ik-12@mail.ru>
2016-05-04 00:33:52 +0400

    исправлен tbitfield.cxx

--- modules/baydina-olya-bitfield/src/tbitfield.cxx
+++ modules/baydina-olya-bitfield/src/tbitfield.cxx
@@ -1,169 +1,154 @@
 // Copyright 2016 Baydina Olya
 
 #include "include/tbitfield.h"
+#include <string>
 using std::string;
 
 int BYTE = 8;
 
-TBitField::TBitField(int len)
-{
-    if (len <= 0){
+TBitField::TBitField(int len) {
+    if (len <= 0) {
         throw 1;
-    }else{
+    } else {
         BitLen = len;
         int tmp = len / (sizeof(TELEM) * BYTE);
-        if (len % (sizeof(TELEM) * BYTE) == 0){
+        if (len % (sizeof(TELEM) * BYTE) == 0) {
             MemLen = tmp;
-        }else{
+        } else {
             MemLen = tmp + 1;
         }
         pMem = new TELEM[MemLen];
-        for (int i = 0; i < MemLen; i++){
+        for (int i = 0; i < MemLen; i++) {
             pMem[i] = 0;
             }
         }
 }
 
-TBitField::TBitField(const TBitField &bf)
-{
+TBitField::TBitField(const TBitField &bf) {
     BitLen = bf.BitLen;
     MemLen = bf.MemLen;
     pMem = new TELEM[MemLen];
-    for (int i = 0; i < MemLen; i++){
+    for (int i = 0; i < MemLen; i++) {
         pMem[i] = bf.pMem[i];
         }
 }
 
-TBitField::~TBitField()
-{
+TBitField::~TBitField() {
     delete[] pMem;
 }
 
-int TBitField::GetMemIndex(const int n) const
-{
+int TBitField::GetMemIndex(const int n) const {
     return (n / (sizeof(TELEM)*BYTE));
 }
 
-TELEM TBitField::GetMemMask(const int n) const
-{
+TELEM TBitField::GetMemMask(const int n) const {
     return (1 << n);
 }
 
-int TBitField::GetLength(void) const
-{
+int TBitField::GetLength(void) const {
     return BitLen;
 }
 
-int TBitField::GetMemLen(void) const
-{
+int TBitField::GetMemLen(void) const {
     return MemLen;
 }
 
-void TBitField::SetBit(const int n)
-{
-    if (n < 0){
+void TBitField::SetBit(const int n) {
+    if (n < 0) {
         throw 1;
-    }else{
-        if (n > BitLen){
+    } else {
+        if (n > BitLen) {
             throw 2;
-            }else{
+            } else {
                 pMem[GetMemIndex(n)] = pMem[GetMemIndex(n)] | GetMemMask(n);
                  }
     }
 }
 
-void TBitField::ClrBit(const int n)
-{
-    if (n < 0){
+void TBitField::ClrBit(const int n) {
+    if (n < 0) {
         throw 1;
-    }else{
-        if (n > BitLen){
+    } else {
+        if (n > BitLen) {
             throw 2;
-        }else{
+        } else {
             pMem[GetMemIndex(n)] = pMem[GetMemIndex(n)] & (~GetMemMask(n));
              }
 	     }
 }
 
-int TBitField::GetBit(const int n) const
-{
-    if (n < 0){
+int TBitField::GetBit(const int n) const {
+    if (n < 0) {
         throw 1;
-    }else{
-        if (n > BitLen){
+    } else {
+        if (n > BitLen) {
             throw 2;
         }
     }
       return((pMem[GetMemIndex(n)] & (GetMemMask(n))) >> n);
 }
 
-TBitField& TBitField::operator=(const TBitField &bf)
-{
+TBitField& TBitField::operator=(const TBitField &bf) {
     BitLen = bf.BitLen;
     MemLen = bf.MemLen;
     pMem = new TELEM[MemLen];
-    for (int i = 0; i < MemLen; i++){
+    for (int i = 0; i < MemLen; i++) {
         pMem[i] = bf.pMem[i];
     }
     return *this;
 }
 
-bool TBitField::operator==(const TBitField &bf) const
-{
-    if (this == &bf){
+bool TBitField::operator==(const TBitField &bf) const {
+    if (this == &bf) {
         return true;
-    }else{
-        if (BitLen != bf.BitLen){
+    } else {
+        if (BitLen != bf.BitLen) {
             return false;
         }
     }
-    for (int i = 0; i < BitLen; i++){
-        if (GetBit(i) != bf.GetBit(i)){
+    for (int i = 0; i < BitLen; i++) {
+        if (GetBit(i) != bf.GetBit(i)) {
             return false;
         }
     }
     return true;
 }
 
-bool TBitField::operator!=(const TBitField &bf) const
-{
-    if (this == &bf){
+bool TBitField::operator!=(const TBitField &bf) const {
+    if (this == &bf) {
         return false;
-        }else{
-            if (BitLen != bf.BitLen){
+        } else {
+            if (BitLen != bf.BitLen) {
                 return true;
             }
         }
-        for (int i = 0; i < BitLen; i++){
-            if (GetBit(i) != bf.GetBit(i)){
+        for (int i = 0; i < BitLen; i++) {
+            if (GetBit(i) != bf.GetBit(i)) {
                 return true;
             }
         }
         return false;
 }
 
-TBitField TBitField::operator|(const TBitField &bf)
-{
+TBitField TBitField::operator|(const TBitField &bf) {
     TBitField result((BitLen > bf.BitLen) ? BitLen : bf.BitLen);
-    for (int i = 0; i < MemLen; i++){
+    for (int i = 0; i < MemLen; i++) {
         result.pMem[i] = bf.pMem[i] | pMem[i];
     }
     return result;
 }
 
-TBitField TBitField::operator&(const TBitField &bf)
-{
+TBitField TBitField::operator&(const TBitField &bf) {
     TBitField result((BitLen>bf.BitLen) ? BitLen : bf.BitLen);
-    for (int i = 0; i < MemLen; i++){
+    for (int i = 0; i < MemLen; i++) {
         result.pMem[i] = bf.pMem[i] & pMem[i];
     }
     return result;
 }
 
-TBitField TBitField::operator~(void)
-{
+TBitField TBitField::operator~(void) {
     TBitField tempRes(BitLen);
-    for (int i = 0; i < tempRes.GetMemLen(); i++){
+    for (int i = 0; i < tempRes.GetMemLen(); i++) {
         tempRes.pMem[i] = ~pMem[i];
     }
     return tempRes;
--- modules/baydina-olya-bitfield/src/tset.cxx
+++ modules/baydina-olya-bitfield/src/tset.cxx
@@ -1,76 +1,61 @@
 // Copyright 2016 Baydina Olya
 
 #include "include/tset.h"
+#include "include/tbitfield.h"
 
-TSet::TSet(int mp) : BitField(mp), MaxPower(mp)
-{
+TSet::TSet(int mp) : BitField(mp), MaxPower(mp) {
 }
 
-TSet::TSet(const TSet &s) : BitField(s.BitField), MaxPower(s.MaxPower)
-{
+TSet::TSet(const TSet &s) : BitField(s.BitField), MaxPower(s.MaxPower) {
 }
 
-TSet::TSet(const TBitField &bf) : BitField(bf), MaxPower(bf.GetLength())
-{
+TSet::TSet(const TBitField &bf) : BitField(bf), MaxPower(bf.GetLength()) {
 }
 
-TSet::operator TBitField()
-{
+TSet::operator TBitField() {
 	return TBitField(MaxPower);
 }
 
-int TSet::GetMaxPower(void) const 
-{
+int TSet::GetMaxPower(void) const {
 	return MaxPower;
 }
 
-int TSet::IsMember(const int Elem) const 
-{
+int TSet::IsMember(const int Elem) const {
 	return BitField.GetBit(Elem);
 }
 
-void TSet::InsElem(const int Elem)
-{
+void TSet::InsElem(const int Elem) {
 	BitField.SetBit(Elem);
 }
 
-void TSet::DelElem(const int Elem)
-{
+void TSet::DelElem(const int Elem) {
 	BitField.ClrBit(Elem);
 }
 
-TSet& TSet::operator=(const TSet &s)
-{
+TSet& TSet::operator=(const TSet &s) {
 	BitField = s.BitField;
 	MaxPower = s.MaxPower;
 
 	return *this;
 }
 
-int TSet::operator==(const TSet &s) const
-{
+int TSet::operator==(const TSet &s) const {
 	return (BitField == s.BitField);
 }
 
-int TSet::operator!=(const TSet &s) const
-{
+int TSet::operator!=(const TSet &s) const {
 	return (BitField != s.BitField);
 }
 
-TSet TSet::operator+(const TSet &s)
-{
+TSet TSet::operator+(const TSet &s) {
 	return (BitField | s.BitField);
 }
 
-
-
-TSet TSet::operator*(const TSet &s)
-{
+TSet TSet::operator*(const TSet &s) {
 	return (BitField & s.BitField);
 }
 
-TSet TSet::operator~(void)
-{
+TSet TSet::operator~(void) {
 	return (~BitField);
 }
 
