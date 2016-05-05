0f29d280a88e7c653d0c3c25d247fbc546521cce
LeraMarkova <lera960331@hotbox.ru>
2016-05-05 23:26:25 +0300

    справления

--- modules/markova-lera-sort/include/sort.h
+++ modules/markova-lera-sort/include/sort.h
@@ -3,24 +3,24 @@
 #include <stdio.h>
 #include <iostream>
 #include <iomanip>
-using namespace std;
+#include <algorithm> 
 
 class MySort {
-private:
+ private:
     int* intArr_;
     int length_;
     void swap(int i);
-	void merge( int first, int last);
-public:
-	MySort(const int intArr[], int length);
-	~MySort();
-	MySort(const MySort&) = delete;
+    void merge(int first, int last);
+ public:
+    MySort(const int intArr[], int length);
+    ~MySort();
+    MySort(const MySort&) = delete;
     int operator[] (int i) const {return intArr_[i]; }
     bool operator== (const MySort& q) const;
-	void quickSort(int l, int r);
+    void quickSort(int l, int r);
     void selectionSort(void);
     void gnomeSort(void);
     void shakerSort(int Start, int N);
     void pasteSort(void);
-	void mergeSort( int first, int last);
+    void mergeSort(int first, int last);
 };
