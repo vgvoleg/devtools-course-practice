b2c8d0ee9a716560f3bfab5020daed71a93ae1a5
Oleg Ovcharuk <Oleg Ovcharuk>
2016-05-16 00:46:08 +0300

    Я забыл, как сильно я люблю трэвис

--- modules/pcalc/application/pcalc.cpp
+++ modules/pcalc/application/pcalc.cpp
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <string>
 
-#include "include\polynom_calculator.h"
+#include "include/polynom_calculator.h"
 
 int main(int argc, const char** argv) {
     PolynomCalculator app;
